11d66bf9d3c64985fe273a4cf369bb737fbbe3be
Stephan Bösebeck <sb@caluga.de>
2012-05-11 23:49:01 +0200

    bugfix: get() with order

--- test/de/caluga/test/mongo/suite/UpdateTest.java
+++ test/de/caluga/test/mongo/suite/UpdateTest.java
@@ -23,31 +23,31 @@ public class UpdateTest extends MongoTest {
             MorphiumSingleton.get().store(o);
         }
 
-        Query<UncachedObject> q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("value").eq("Uncached "+5);
-        UncachedObject uc=q.get();
-        MorphiumSingleton.get().inc(uc,"counter",1);
+        Query<UncachedObject> q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("value").eq("Uncached " + 5);
+        UncachedObject uc = q.get();
+        MorphiumSingleton.get().inc(uc, "counter", 1);
 
-        assert(uc.getCounter()==6):"Counter is not correct: "+uc.getCounter();
+        assert (uc.getCounter() == 6) : "Counter is not correct: " + uc.getCounter();
 
         //inc without object - single update, no upsert
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gte(10).f("counter").lte(25).order("-counter");
-        MorphiumSingleton.get().inc(UncachedObject.class,q,"counter",100);
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gte(10).f("counter").lte(25).order("counter");
+        MorphiumSingleton.get().inc(UncachedObject.class, q, "counter", 100);
 
-        uc=q.get();
-        assert(uc.getCounter()==11):"Counter is wrong: "+uc.getCounter();
+        uc = q.get();
+        assert (uc.getCounter() == 11) : "Counter is wrong: " + uc.getCounter();
 
         //inc without object directly in DB - multiple update
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gt(10).f("counter").lte(25);
-        MorphiumSingleton.get().inc(UncachedObject.class,q,"counter",100,false,true);
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gt(10).f("counter").lte(25);
+        MorphiumSingleton.get().inc(UncachedObject.class, q, "counter", 100, false, true);
 
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gt(110).f("counter").lte(125);
-        List<UncachedObject> lst=q.asList(); //read the data after update
-        for (UncachedObject u:lst) {
-            assert(u.getCounter()>110 && u.getCounter()<=125 && u.getValue().equals("Uncached "+(u.getCounter()-100))):"Counter wrong: "+u.getCounter();
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gt(110).f("counter").lte(125);
+        List<UncachedObject> lst = q.asList(); //read the data after update
+        for (UncachedObject u : lst) {
+            assert (u.getCounter() > 110 && u.getCounter() <= 125 && u.getValue().equals("Uncached " + (u.getCounter() - 100))) : "Counter wrong: " + u.getCounter();
         }
 
     }
@@ -61,31 +61,31 @@ public class UpdateTest extends MongoTest {
             MorphiumSingleton.get().store(o);
         }
 
-        Query<UncachedObject> q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("value").eq("Uncached "+5);
-        UncachedObject uc=q.get();
-        MorphiumSingleton.get().dec(uc,"counter",1);
+        Query<UncachedObject> q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("value").eq("Uncached " + 5);
+        UncachedObject uc = q.get();
+        MorphiumSingleton.get().dec(uc, "counter", 1);
 
-        assert(uc.getCounter()==4):"Counter is not correct: "+uc.getCounter();
+        assert (uc.getCounter() == 4) : "Counter is not correct: " + uc.getCounter();
 
         //inc without object - single update, no upsert
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gte(40).f("counter").lte(55).order("-counter");
-        MorphiumSingleton.get().dec(UncachedObject.class,q,"counter",40);
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gte(40).f("counter").lte(55).order("counter");
+        MorphiumSingleton.get().dec(UncachedObject.class, q, "counter", 40);
 
-        uc=q.get();
-        assert(uc.getCounter()==41):"Counter is wrong: "+uc.getCounter();
+        uc = q.get();
+        assert (uc.getCounter() == 41) : "Counter is wrong: " + uc.getCounter();
 
         //inc without object directly in DB - multiple update
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gt(40).f("counter").lte(55);
-        MorphiumSingleton.get().dec(UncachedObject.class,q,"counter",40,false,true);
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gt(40).f("counter").lte(55);
+        MorphiumSingleton.get().dec(UncachedObject.class, q, "counter", 40, false, true);
 
-        q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("counter").gt(0).f("counter").lte(55);
-        List<UncachedObject> lst=q.asList(); //read the data after update
-        for (UncachedObject u:lst) {
-            assert(u.getCounter()>0 && u.getCounter()<=55 ):"Counter wrong: "+u.getCounter();
+        q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("counter").gt(0).f("counter").lte(55);
+        List<UncachedObject> lst = q.asList(); //read the data after update
+        for (UncachedObject u : lst) {
+            assert (u.getCounter() > 0 && u.getCounter() <= 55) : "Counter wrong: " + u.getCounter();
 //            assert(u.getValue().equals("Uncached "+(u.getCounter()-40))):"Value wrong: Counter: "+u.getCounter()+" Value;: "+u.getValue();
         }
 
@@ -100,12 +100,12 @@ public class UpdateTest extends MongoTest {
             MorphiumSingleton.get().store(o);
         }
 
-        Query<UncachedObject> q=MorphiumSingleton.get().createQueryFor(UncachedObject.class);
-        q=q.f("value").eq("unexistent");
-        MorphiumSingleton.get().set(UncachedObject.class,q,"counter",999,true,false);
-        UncachedObject uc=q.get(); //should now work
+        Query<UncachedObject> q = MorphiumSingleton.get().createQueryFor(UncachedObject.class);
+        q = q.f("value").eq("unexistent");
+        MorphiumSingleton.get().set(UncachedObject.class, q, "counter", 999, true, false);
+        UncachedObject uc = q.get(); //should now work
 
-        assert(uc!=null):"Not found?!?!?";
-        assert(uc.getValue().equals("unexistent")):"Value wrong: "+uc.getValue();
+        assert (uc != null) : "Not found?!?!?";
+        assert (uc.getValue().equals("unexistent")) : "Value wrong: " + uc.getValue();
     }
 }
