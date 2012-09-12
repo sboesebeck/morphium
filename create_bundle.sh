dc4beb1c4260c481596a82f3d5ee219884da509b
Stephan Bösebeck <sb@caluga.de>
2012-09-12 11:33:05 +0200

    bundle creation implemented

---
+++ create_bundle.sh
@@ -0,0 +1,6 @@
+mvn package gpg:sign -Dgpg.passphrase="$GPGPWD" -Dmaven.test.skip=true $@ || exit 1
+echo "creating bundle"
+version=$(grep '<version>' pom.xml | head -n1 | tr -d ' a-z<>/')
+echo "Version $version"
+cd target
+jar -cvf bundle_$version.jar morphium-$version* 
