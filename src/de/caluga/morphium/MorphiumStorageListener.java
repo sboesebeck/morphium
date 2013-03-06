4a321596b45851e09eba224adf881900122649ab
Stephan Bösebeck <sb@caluga.de>
2013-03-06 08:32:15 +0100

    validation of embedded types

--- src/de/caluga/morphium/MorphiumStorageListener.java
+++ src/de/caluga/morphium/MorphiumStorageListener.java
@@ -15,26 +15,26 @@ public interface MorphiumStorageListener<T> {
         SET, UNSET, PUSH, PULL, INC, DEC;
     }
 
-    public void preStore(T r, boolean isNew);
+    public void preStore(Morphium m, T r, boolean isNew);
 
-    public void postStore(T r, boolean isNew);
+    public void postStore(Morphium m, T r, boolean isNew);
 
-    public void postRemove(T r);
+    public void postRemove(Morphium m, T r);
 
-    public void preDelete(T r);
+    public void preDelete(Morphium m, T r);
 
-    public void postDrop(Class<? extends T> cls);
+    public void postDrop(Morphium m, Class<? extends T> cls);
 
-    public void preDrop(Class<? extends T> cls);
+    public void preDrop(Morphium m, Class<? extends T> cls);
 
-    public void preRemove(Query<T> q);
+    public void preRemove(Morphium m, Query<T> q);
 
-    public void postRemove(Query<T> q);
+    public void postRemove(Morphium m, Query<T> q);
 
-    public void postLoad(T o);
+    public void postLoad(Morphium m, T o);
 
-    public void preUpdate(Class<? extends T> cls, Enum updateType);
+    public void preUpdate(Morphium m, Class<? extends T> cls, Enum updateType);
 
-    public void postUpdate(Class<? extends T> cls, Enum updateType);
+    public void postUpdate(Morphium m, Class<? extends T> cls, Enum updateType);
 
 }
--- src/de/caluga/morphium/validation/JavaxValidationStorageListener.java
+++ src/de/caluga/morphium/validation/JavaxValidationStorageListener.java
@@ -1,17 +1,20 @@
 package de.caluga.morphium.validation;
 
-import de.caluga.morphium.MorphiumStorageListener;
-import de.caluga.morphium.query.Query;
+import de.caluga.morphium.Morphium;
+import de.caluga.morphium.MorphiumStorageAdapter;
+import de.caluga.morphium.annotations.Embedded;
+import de.caluga.morphium.annotations.Entity;
+import org.apache.log4j.Logger;
 
 import javax.validation.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * User: martinstolz
  * Date: 29.08.12
  */
-public class JavaxValidationStorageListener implements MorphiumStorageListener<Object> {
+public class JavaxValidationStorageListener extends MorphiumStorageAdapter<Object> {
 
     private Validator validator;
 
@@ -21,52 +24,61 @@ public class JavaxValidationStorageListener implements MorphiumStorageListener<O
     }
 
     @Override
-    public void preStore(Object r, boolean isNew) {
+    public void preStore(Morphium m, Object r, boolean isNew) {
+        if (r == null) return;
+        if (!m.isAnnotationPresentInHierarchy(r.getClass(), Entity.class) && !m.isAnnotationPresentInHierarchy(r.getClass(), Embedded.class)) {
+            return;
+        }
         Set<ConstraintViolation<Object>> violations = validator.validate(r);
+
+        List<String> flds = m.getFields(r.getClass());
+        for (String f : flds) {
+            Field field = m.getField(r.getClass(), f);
+            if (m.isAnnotationPresentInHierarchy(field.getType(), Embedded.class) ||
+                    m.isAnnotationPresentInHierarchy(field.getType(), Entity.class)) {
+                //also check it
+                try {
+                    Set<ConstraintViolation<Object>> v = validator.validate(field.get(r));
+                    violations.addAll(v);
+                } catch (IllegalAccessException e) {
+                    Logger.getLogger(JavaxValidationStorageListener.class).error("Could not access Field: " + f);
+                }
+            } else if (Collection.class.isAssignableFrom(field.getType())) {
+                //list handling
+                try {
+                    Collection<Object> lst = (List<Object>) field.get(r);
+                    for (Object o : lst) {
+                        try {
+                            preStore(m, o, isNew);
+                        } catch (ConstraintViolationException e) {
+                            violations.addAll((Set<ConstraintViolation<Object>>) e.getConstraintViolations());
+                        }
+                    }
+
+                } catch (IllegalAccessException e) {
+                    Logger.getLogger(JavaxValidationStorageListener.class).error("Could not access list field: " + f);
+                }
+            } else if (Map.class.isAssignableFrom(field.getType())) {
+                //usually only strings are allowed as keys - especially no embedded or entitiy types
+                //just checking values
+                try {
+                    Map map = (Map) field.get(r);
+                    for (Object val : map.values()) {
+                        try {
+                            preStore(m, val, isNew);
+                        } catch (ConstraintViolationException e) {
+                            violations.addAll((Set<ConstraintViolation<Object>>) e.getConstraintViolations());
+                        }
+                    }
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
         if (violations.size() > 0) {
             throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
         }
     }
 
-    // do nothing methods down below
-
-    @Override
-    public void postStore(Object r, boolean isNew) {
-    }
-
-    @Override
-    public void postRemove(Object r) {
-    }
-
-    @Override
-    public void preDelete(Object r) {
-    }
-
-    @Override
-    public void postDrop(Class<?> cls) {
-    }
-
-    @Override
-    public void preDrop(Class<?> cls) {
-    }
-
-    @Override
-    public void preRemove(Query<Object> q) {
-    }
-
-    @Override
-    public void postRemove(Query<Object> q) {
-    }
-
-    @Override
-    public void postLoad(Object o) {
-    }
-
-    @Override
-    public void preUpdate(Class<?> cls, Enum updateType) {
-    }
-
-    @Override
-    public void postUpdate(Class<?> cls, Enum updateType) {
-    }
 }
