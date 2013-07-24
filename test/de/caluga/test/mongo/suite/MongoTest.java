6c8ebb8431f02301b8b6ab5b24ada1690d802b06
Stephan Bösebeck <sb@caluga.de>
2013-07-24 16:19:32 +0200

    removing auth from test

--- test/de/caluga/test/mongo/suite/MongoTest.java
+++ test/de/caluga/test/mongo/suite/MongoTest.java
@@ -103,9 +103,9 @@ public class MongoTest {
 //            cfg.setTimeoutBugWorkAroundEnabled(true);
 
 
-            cfg.addAddress("192.168.44.31", 27017);
-            cfg.addAddress("192.168.44.31", 27018);
-            cfg.addAddress("192.168.44.31", 27019);
+            cfg.addAddress("localhost", 27017);
+            cfg.addAddress("localhost", 27018);
+            cfg.addAddress("localhost", 27019);
 //            cfg.addAddress("mongo1", 27017);
 //            cfg.addAddress("mongo2", 27017);
 //            cfg.addAddress("mongo3", 27017);
@@ -120,11 +120,11 @@ public class MongoTest {
             cfg.setRetryWaitTimeWriter(1000);
             cfg.setRetryWaitTimeBufferedWriter(1000);
 
-            cfg.setMongoAdminUser("admin");
-            cfg.setMongoAdminPwd("admin");
-
-            cfg.setMongoLogin("tst");
-            cfg.setMongoPassword("tst");
+//            cfg.setMongoAdminUser("admin");
+//            cfg.setMongoAdminPwd("admin");
+//
+//            cfg.setMongoLogin("tst");
+//            cfg.setMongoPassword("tst");
 //            cfg.setMongoLogin("morphium");
 //            cfg.setMongoPassword("tst");
 
