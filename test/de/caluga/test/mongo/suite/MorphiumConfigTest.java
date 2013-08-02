bda483ad28a271919db6957cc464b4cd1f361d98
Stephan Bösebeck <sb@caluga.de>
2013-08-02 10:30:29 +0200

    bugfixes with config, storing test config in property file

--- test/de/caluga/test/mongo/suite/MorphiumConfigTest.java
+++ test/de/caluga/test/mongo/suite/MorphiumConfigTest.java
@@ -24,10 +24,67 @@ public class MorphiumConfigTest extends MongoTest {
     }
 
     @Test
+    public void testDefaultProps() throws Exception {
+        MorphiumConfig cfg = new MorphiumConfig();
+        Properties p = cfg.asProperties();
+        for (Object k : p.keySet()) {
+            log.info("Key: " + k + " Value: " + p.get(k));
+        }
+        p.store(System.out, "testproperties");
+
+        String cfgStr = cfg.toString();
+        log.info("Got: " + cfgStr);
+
+    }
+
+    @Test
+    public void partialJsonTest() throws Exception {
+        String json = "{ \"hosts\":\"mongo1:27018, mongo2:27099\", \"database\" : \"testdb\", \"safe_mode\" : true , \"global_fsync\" : false , \"globalJ\" : false , \"write_timeout\" : 9990 }";
+        MorphiumConfig cfg = MorphiumConfig.createFromJson(json);
+        assert (cfg.getWriteTimeout() == 9990);
+        assert (cfg.isSafeMode());
+        assert (cfg.getDatabase().equals("testdb"));
+        assert (cfg.getAdr().size() == 2);
+        assert (cfg.getAdr().get(0).getPort() == 27018);
+        assert (cfg.getAdr().get(1).getPort() == 27099);
+
+    }
+
+    @Test
+    public void testHosts() throws Exception {
+        MorphiumConfig cfg = new MorphiumConfig();
+        cfg.addHost("localhost:9999");
+        cfg.addHost("localhost", 1000);
+        assert (cfg.getAdr().size() == 2);
+
+        cfg.setHosts("localhost:9999,localhost:2222,localhost:2344");
+        assert (cfg.getAdr().size() == 3);
+
+        cfg.setHosts("localhost,localhost,localhost,localhost", "1, 2,   3,4");
+        assert (cfg.getAdr().size() == 4);
+
+    }
+
+
+    @Test
+    public void partialPropsTest() throws Exception {
+        Properties p = new Properties();
+        p.put("maximumRetriesAsyncWriter", "10");
+        p.put("socketTimeout", "1000");
+        p.put("database", "thingy");
+        p.put("hosts", "localhost:27017");
+
+        MorphiumConfig cfg = MorphiumConfig.fromProperties(p);
+        assert (cfg.getAdr().size() == 1);
+        assert (cfg.getDatabase().equals("thingy"));
+        assert (cfg.getSocketTimeout() == 1000);
+    }
+
+    @Test
     public void testToProperties() throws Exception {
         Properties p = MorphiumSingleton.get().getConfig().asProperties();
         for (Object k : p.keySet()) {
-            log.info("KEy: " + k + " Value: " + p.get(k));
+            log.info("Key: " + k + " Value: " + p.get(k));
         }
         p.store(System.out, "testproperties");
 
