2d8bcb6e055e62f1c8a7a4f59f2b64ec4b8ffb3e
Stephan Bösebeck <sb@caluga.de>
2013-11-06 15:08:03 +0100

    added test for expireAfterSeconds-Index

---
+++ test/de/caluga/test/mongo/suite/ExpireIndexTest.java
@@ -0,0 +1,47 @@
+package de.caluga.test.mongo.suite;
+
+import de.caluga.morphium.MorphiumSingleton;
+import de.caluga.morphium.annotations.CreationTime;
+import de.caluga.morphium.annotations.Index;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stephan
+ * Date: 06.11.13
+ * Time: 14:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExpireIndexTest extends MongoTest {
+    @Test
+    public void testExpiry() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            UCobj u = new UCobj();
+            u.setCounter(i);
+            u.setValue("V" + i);
+            MorphiumSingleton.get().store(u);
+        }
+        assert (MorphiumSingleton.get().createQueryFor(UCobj.class).countAll() == 200);
+        log.info("Waiting for mongo to clear it");
+        Thread.sleep(65000);
+        assert (MorphiumSingleton.get().createQueryFor(UCobj.class).countAll() == 0);
+    }
+
+    @Index(value = {"created"}, options = {"expireAfterSeconds:5"})
+    @CreationTime
+    public static class UCobj extends UncachedObject {
+
+        @CreationTime
+        private Date created;
+
+        public Date getCreated() {
+            return created;
+        }
+
+        public void setCreated(Date created) {
+            this.created = created;
+        }
+    }
+}
