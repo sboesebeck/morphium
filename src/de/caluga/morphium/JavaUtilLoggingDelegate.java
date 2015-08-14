192d877923cdc259943faf2e1d89fcd53776c565
Stephan Bösebeck <sb@caluga.de>
2015-08-14 15:03:57 +0200

    logger funktionality added for use of log4j and util.logging Configure it with morphium.og.delegate=log4j, jul or your class implementing LoggerDelegate

---
+++ src/de/caluga/morphium/JavaUtilLoggingDelegate.java
@@ -0,0 +1,37 @@
+package de.caluga.morphium;/**
+ * Created by stephan on 14.08.15.
+ */
+
+import java.util.logging.Level;
+
+/**
+ * TODO: Add Documentation here
+ **/
+public class JavaUtilLoggingDelegate implements LoggerDelegate {
+    @Override
+    public void log(String loggerName, int lv, String msg, Throwable t) {
+        java.util.logging.Level level;
+
+        switch (lv) {
+            case 0:
+                return;
+            case 1:
+            case 2:
+                level = Level.SEVERE;
+                break;
+            case 3:
+                level = Level.WARNING;
+                break;
+            case 4:
+                level = Level.INFO;
+                break;
+            case 5:
+                level = Level.ALL;
+                break;
+            default:
+                level = Level.ALL;
+                break;
+        }
+        java.util.logging.Logger.getLogger(loggerName).log(level, msg, t);
+    }
+}
