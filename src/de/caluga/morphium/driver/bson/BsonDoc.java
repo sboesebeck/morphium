795c2a67966338442acccaf63390218609e87d94
Stephan Bösebeck <sb@caluga.de>
2015-10-26 23:06:08 +0100

    bsonencoder...

---
+++ src/de/caluga/morphium/driver/bson/BsonDoc.java
@@ -0,0 +1,74 @@
+package de.caluga.morphium.driver.bson;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * User: Stephan Bösebeck
+ * Date: 26.10.15
+ * Time: 22:46
+ * <p>
+ * TODO: Add documentation here
+ */
+public class BsonDoc implements Map {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
+    }
+}
