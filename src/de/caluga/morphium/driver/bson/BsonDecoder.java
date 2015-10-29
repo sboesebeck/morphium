ef05e4a368ea63417615fc3b1e25ded84ad45a8b
Stephan Bösebeck <sb@caluga.de>
2015-10-29 22:42:25 +0100

    BsonDecoder

---
+++ src/de/caluga/morphium/driver/bson/BsonDecoder.java
@@ -0,0 +1,15 @@
+package de.caluga.morphium.driver.bson;/**
+ * Created by stephan on 29.10.15.
+ */
+
+import java.util.Map;
+
+/**
+ * TODO: Add Documentation here
+ **/
+public class BsonDecoder {
+
+    public Map decodeDocument(byte[] in) {
+
+    }
+}
