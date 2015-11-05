517eba5c2d99a50d07d60f8b6977fd862e02b95b
Stephan Bösebeck <sb@caluga.de>
2015-11-05 22:01:53 +0100

    Driver implementation preparation

--- src/de/caluga/morphium/driver/MorphiumDb.java
+++ src/de/caluga/morphium/driver/MorphiumDb.java
@@ -2,7 +2,7 @@ package de.caluga.morphium.driver;/**
  * Created by stephan on 16.10.15.
  */
 
-import de.caluga.morphium.annotations.ReadPreferenceLevel;
+import java.util.Map;
 
 /**
  * TODO: Add Documentation here
@@ -12,35 +12,22 @@ public interface MorphiumDb {
 
     String getName();
 
-    void setDefaultReadPreference(ReadPreferenceLevel l);
+    void setDefaultReadPreference(ReadPreference rp);
 
-    ReadPreferenceLevel getDefaultReadPreference();
+    ReadPreference getDefaultReadPreference();
 
-    void setDefaultWriteSafety();
+    void setDefaultWriteConcern(WriteConcern wc);
+
+    WriteConcern getDefaultWriteConcern();
 
 
-    void dropDatabase();
+    void drop();
 
-    MorphiumCollection createCollection(String name, MongoDocument options);
+    MorphiumCollection createCollection(String name, Map<String, Object> options);
 
-    MongoDocument command(MongoDocument cmd);
+    Map<String, Object> command(Map<String, Object> cmd);
 
     boolean existsCollection(String coll);
 
 
-    /**
-     * bit num	name	description
-     * 0	Reserved	Must be set to 0.
-     * 1	TailableCursor	Tailable means cursor is not closed when the last data is retrieved. Rather, the cursor marks the final object’s position. You can resume using the cursor later, from where it was located, if more data were received. Like any “latent cursor”, the cursor may become invalid at some point (CursorNotFound) – for example if the final object it references were deleted.
-     * 2	SlaveOk	Allow query of replica slave. Normally these return an error except for namespace “local”.
-     * 3	OplogReplay	Internal replication use only - driver should not set
-     * 4	NoCursorTimeout	The server normally times out idle cursors after an inactivity period (10 minutes) to prevent excess memory use. Set this option to prevent that.
-     * 5	AwaitData	Use with TailableCursor. If we are at the end of the data, block for a while rather than returning no data. After a timeout period, we do return as normal.
-     * 6	Exhaust	Stream the data down full blast in multiple “more” packages, on the assumption that the client will fully read all data queried. Faster when you are pulling a lot of data and know you want to pull it all down. Note: the client is not allowed to not read all the data unless it closes the connection.
-     * 7	Partial	Get partial results from a mongos if some shards are down (instead of throwing an error)
-     * 8-31	Reserved	Must be set to 0.
-     */
-    int getFlags();
-
-    void setFlags(int f);
 }
---
+++ src/de/caluga/morphium/driver/wireprotocol/OpReply.java
@@ -0,0 +1,139 @@
+package de.caluga.morphium.driver.wireprotocol;/**
+ * Created by stephan on 04.11.15.
+ */
+
+import de.caluga.morphium.driver.bson.BsonDecoder;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * TODO: Add Documentation here
+ **/
+
+public class OpReply extends WireProtocolMessage {
+    int reqId;
+    int inReplyTo;
+
+    int size = 0;
+    int opcode = 1;
+    int flags;
+    long cursorId;
+    int startFrom;
+    int numReturned;
+    List<Map<String, Object>> documents;
+
+
+    public void parse(byte[] bytes) throws UnsupportedEncodingException {
+        parse(bytes, 0);
+    }
+
+    public int getReqId() {
+        return reqId;
+    }
+
+    public void setReqId(int reqId) {
+        this.reqId = reqId;
+    }
+
+    public int getInReplyTo() {
+        return inReplyTo;
+    }
+
+    public void setInReplyTo(int inReplyTo) {
+        this.inReplyTo = inReplyTo;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getOpcode() {
+        return opcode;
+    }
+
+    public void setOpcode(int opcode) {
+        this.opcode = opcode;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+
+    public void setFlags(int flags) {
+        this.flags = flags;
+    }
+
+    public long getCursorId() {
+        return cursorId;
+    }
+
+    public void setCursorId(long cursorId) {
+        this.cursorId = cursorId;
+    }
+
+    public int getStartFrom() {
+        return startFrom;
+    }
+
+    public void setStartFrom(int startFrom) {
+        this.startFrom = startFrom;
+    }
+
+    public int getNumReturned() {
+        return numReturned;
+    }
+
+    public void setNumReturned(int numReturned) {
+        this.numReturned = numReturned;
+    }
+
+    public List<Map<String, Object>> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<Map<String, Object>> documents) {
+        this.documents = documents;
+    }
+
+    private void parse(byte[] bytes, int offset) throws UnsupportedEncodingException {
+        size = readInt(bytes, offset);
+        offset += 4;
+        reqId = readInt(bytes, offset);
+        offset += 4;
+        inReplyTo = readInt(bytes, offset);
+        offset += 4;
+        opcode = readInt(bytes, offset);
+        offset += 4;
+        if (opcode != 1) throw new IllegalArgumentException("Unknown Opcode " + opcode);
+        flags = readInt(bytes, offset);
+        offset += 4;
+        cursorId = readLong(bytes, offset);
+        offset += 8;
+        startFrom = readInt(bytes, offset);
+        offset += 4;
+        numReturned = readInt(bytes, offset);
+        offset += 4;
+
+        documents = new ArrayList<>();
+        for (int i = 0; i < numReturned; i++) {
+            Map<String, Object> m = new HashMap<>();
+            BsonDecoder dec = new BsonDecoder();
+            int l = dec.decodeDocumentIn(m, bytes, offset);
+            offset += l;
+            documents.add(m);
+        }
+
+    }
+
+    public int getOpCode() {
+        return opcode;
+    }
+}
