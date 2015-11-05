d2ac292eda95dcf035070f1fcdc0806de0425f60
Stephan Bösebeck <sb@caluga.de>
2015-11-05 22:01:53 +0100

    Driver implementation preparation

--- src/de/caluga/morphium/driver/MorphiumCollection.java
+++ src/de/caluga/morphium/driver/MorphiumCollection.java
@@ -2,8 +2,39 @@ package de.caluga.morphium.driver;/**
  * Created by stephan on 15.10.15.
  */
 
+
 /**
  * TODO: Add Documentation here
  **/
 public class MorphiumCollection {
+
+    private String collection;
+
+    private ReadPreference readPreference;
+    private WriteConcern writeConcern;
+
+
+    public WriteConcern getWriteConcern() {
+        return writeConcern;
+    }
+
+    public void setWriteConcern(WriteConcern writeConcern) {
+        this.writeConcern = writeConcern;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+
+    public ReadPreference getReadPreference() {
+        return readPreference;
+    }
+
+    public void setReadPreference(ReadPreference readPreference) {
+        this.readPreference = readPreference;
+    }
 }
