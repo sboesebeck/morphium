2c3a3771db7e4c796e2c28546a82959539485feb
Stephan Bösebeck <sb@caluga.de>
2015-11-13 16:52:50 +0100

    working on Bulk Operations - not compiling now...

--- src/de/caluga/morphium/driver/bulk/BulkRequestContext.java
+++ src/de/caluga/morphium/driver/bulk/BulkRequestContext.java
@@ -2,13 +2,16 @@ package de.caluga.morphium.driver.bulk;/**
  * Created by stephan on 13.11.15.
  */
 
+import de.caluga.morphium.driver.MorphiumDriver;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * TODO: Add Documentation here
  **/
-public class BulkRequestContext {
+public abstract class BulkRequestContext {
     private List<BulkRequest> requests = new ArrayList<>();
 
     private boolean odererd = false;
@@ -16,5 +19,7 @@ public class BulkRequestContext {
     public void addRequest(BulkRequest br) {
         requests.add(br);
     }
+
+    public abstract Map<String, Object> execute(MorphiumDriver drv);
 
 }
