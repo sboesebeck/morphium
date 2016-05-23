8136892af34935253fd8e14a8fbee3bd45f97c7f
FroznMine <froznmine@pesacraft.de>
2016-05-24 00:22:45 +0200

    Create custom mapper test and needed data objects
    The test consists of two tests:
     The first, customMappedObjecTest, is used to find out whether the
    (un-)marshaling works for objects mapped with a custom mapper. It will
    try to save the object as a property, as part of a list and as a value
    in a map.
    The second, complexCustomMappingTest, goes a step further and tries to
    (un-)marshal a list containing a map with a custom mapped object as
    well as a map containing a list with a custom mapped object as value.
    This test is used to guarantee that the mapping works over multiple
    layers. The ‚complexest‘ list and map are used to prove the possibility
    of recursion when mapping the objects. This may be a bit far fetched
    and not fitting perfectly here. It could be put into the complex test
    section somehow. Anyway this tests guarantee that custom mapped objects
    will be unmapped no matter where they are.

---
+++ test/de/caluga/test/mongo/suite/data/CustomMappedObject.java
@@ -0,0 +1,48 @@
+package de.caluga.test.mongo.suite.data;
+
+public class CustomMappedObject {
+	private String name;
+	private String value;
+	private int intValue;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public int getIntValue() {
+		return intValue;
+	}
+	
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof CustomMappedObject))
+			return false;
+		
+		CustomMappedObject that = (CustomMappedObject) obj;
+		
+		if (!this.getName().equals(that.getName()))
+			return false;
+		if (!this.getValue().equals(that.getValue()))
+			return false;
+		if (this.getIntValue() != that.getIntValue())
+			return false;
+		
+		return true;
+	}
+}
---
+++ test/de/caluga/test/mongo/suite/data/CustomMappedObjectMapper.java
@@ -0,0 +1,34 @@
+package de.caluga.test.mongo.suite.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.caluga.morphium.TypeMapper;
+
+public class CustomMappedObjectMapper implements TypeMapper<CustomMappedObject> {
+
+	@Override
+	public Object marshall(CustomMappedObject o) {
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("name", o.getName());
+		map.put("string_value", o.getValue());
+		map.put("int_value", o.getIntValue());
+		return map;
+	}
+
+	@Override
+	public CustomMappedObject unmarshall(Object d) {
+		if (d instanceof Map) {
+			Map<String, Object> map = (Map<String, Object>) d;
+			
+			CustomMappedObject cmo = new CustomMappedObject();
+			cmo.setName((String) map.get("name"));
+			cmo.setValue((String) map.get("string_value"));
+			cmo.setIntValue((int) map.get("int_value"));
+			return cmo;
+		}
+		return null;
+	}
+
+}
