47e8dd478b6691196880bfca5cebf1416d4011f9
Stephan Bösebeck <sb@caluga.de>
2016-07-07 09:49:47 +0200

    fixing wilcard & object lists with different elementtypes

--- test/de/caluga/test/mongo/suite/ListTests.java
+++ test/de/caluga/test/mongo/suite/ListTests.java
@@ -1,13 +1,20 @@
 package de.caluga.test.mongo.suite;
 
+import de.caluga.morphium.annotations.Entity;
+import de.caluga.morphium.annotations.Id;
 import de.caluga.morphium.annotations.ReadPreferenceLevel;
+import de.caluga.morphium.driver.bson.MorphiumId;
 import de.caluga.morphium.query.Query;
 import de.caluga.test.mongo.suite.data.EmbeddedObject;
+import de.caluga.test.mongo.suite.data.ExtendedEmbeddedObject;
+import de.caluga.test.mongo.suite.data.ListWildcardContainer;
 import de.caluga.test.mongo.suite.data.UncachedObject;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.fail;
 
 /**
  * User: Stephan Bösebeck
@@ -144,4 +151,92 @@ public class ListTests extends MongoTest {
 
     }
 
+    @Test
+    public void testWildcardList() {
+        morphium.dropCollection(ListWildcardContainer.class);
+
+        ListWildcardContainer testObjectToStore = new ListWildcardContainer();
+        testObjectToStore.setId(new MorphiumId("100000000000000000000001"));
+        List<EmbeddedObject> embeddedObjectList = (List<EmbeddedObject>) testObjectToStore.getEmbeddedObjectList();
+
+        ExtendedEmbeddedObject extendedEmbeddedObject = new ExtendedEmbeddedObject();
+        extendedEmbeddedObject.setName("testName");
+        extendedEmbeddedObject.setAdditionalValue("additonalValue");
+        extendedEmbeddedObject.setTest(4711);
+        extendedEmbeddedObject.setValue("value");
+
+        embeddedObjectList.add(extendedEmbeddedObject);
+
+        morphium.store(testObjectToStore);
+
+        Query q = morphium.createQueryFor(ListWildcardContainer.class).f("id").eq(testObjectToStore.getId());
+
+        try {
+            ListWildcardContainer testObejectToLoadFromDB = (ListWildcardContainer) q.get();
+        } catch (Exception e) {
+            fail("error while restoring object from db | " + e.getMessage());
+            log.error(e);
+        }
+    }
+
+    @Test
+    public void testHybridList() {
+        morphium.dropCollection(MyListContainer.class);
+        MyListContainer mc = new MyListContainer();
+        mc.name = "test";
+        mc.number = 42;
+        mc.objectList = new ArrayList<>();
+
+        ExtendedEmbeddedObject extendedEmbeddedObject = new ExtendedEmbeddedObject();
+        extendedEmbeddedObject.setName("testName");
+        extendedEmbeddedObject.setAdditionalValue("additionalValue");
+        extendedEmbeddedObject.setTest(4711);
+        extendedEmbeddedObject.setValue("value");
+
+        UncachedObject uc = new UncachedObject();
+        uc.setCounter(42);
+        uc.setValue("val");
+
+        EmbeddedObject eo = new EmbeddedObject();
+        eo.setValue("Embedded");
+        eo.setName("Fred");
+        eo.setTest(System.currentTimeMillis());
+
+        mc.objectList.add(uc);
+        mc.objectList.add(eo);
+        mc.objectList.add(extendedEmbeddedObject);
+
+        morphium.store(mc);
+
+        MyListContainer mc2 = morphium.createQueryFor(MyListContainer.class).asList().get(0);
+        assert (mc2.id.equals(mc.id));
+        assert (mc2.objectList.size() == mc.objectList.size());
+
+        assert (mc2.objectList.get(0) instanceof UncachedObject);
+        assert (mc2.objectList.get(1) instanceof EmbeddedObject);
+        assert (mc2.objectList.get(2) instanceof ExtendedEmbeddedObject);
+
+        assert (((UncachedObject) mc2.objectList.get(0)).getValue().equals("val"));
+        assert (((UncachedObject) mc2.objectList.get(0)).getCounter() == 42);
+
+        assert (((EmbeddedObject) mc2.objectList.get(1)).getValue().equals("Embedded"));
+        assert (((EmbeddedObject) mc2.objectList.get(1)).getName().equals("Fred"));
+        assert (((EmbeddedObject) mc2.objectList.get(1)).getTest() != 0);
+
+        assert (((ExtendedEmbeddedObject) mc2.objectList.get(2)).getName().equals("testName"));
+        assert (((ExtendedEmbeddedObject) mc2.objectList.get(2)).getAdditionalValue().equals("additionalValue"));
+        assert (((ExtendedEmbeddedObject) mc2.objectList.get(2)).getTest() == 4711);
+        assert (((ExtendedEmbeddedObject) mc2.objectList.get(2)).getValue().equals("value"));
+
+
+    }
+
+    @Entity
+    public static class MyListContainer {
+        @Id
+        public MorphiumId id;
+        public List<Object> objectList;
+        public String name;
+        public int number;
+    }
 }
