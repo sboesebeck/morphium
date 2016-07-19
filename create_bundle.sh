7c008a92c22a032509f29c58249182e1ae23c9d8
Stephan Bösebeck <sb@caluga.de>
2016-07-19 09:08:11 +0200

    fix typo

--- create_bundle.sh
+++ create_bundle.sh
@@ -4,4 +4,4 @@ echo "creating bundle"
 version=$(grep '<version>' pom.xml | head -n1 | tr -d ' a-z<>/')
 echo "Version $version"
 cd target
-jar -cvf bundle_${version}.jar bundle_${version}.jar.asc morphium-${version}.pom morphium-${version}.pom.asc morphium-${version}.jar.asc morphium-${version}.jar morphium-${version}-javadoc.jar morphium-${version}-javadoc.jar.asc morphium-${version}-sources.jar.asc morphium-${version}-sources.jar
+jar -cvf bundle_${version}.jar morphium-${version}.pom morphium-${version}.pom.asc morphium-${version}.jar.asc morphium-${version}.jar morphium-${version}-javadoc.jar morphium-${version}-javadoc.jar.asc morphium-${version}-sources.jar.asc morphium-${version}-sources.jar
