059ec1959409d5dc644c79467c437ac23eee4e03
Stephan Bösebeck <stephan@boesebeck.biz>
2018-11-13 10:17:39 +0100

    fixing some tests

--- runtests.sh
+++ runtests.sh
@@ -3,7 +3,7 @@ function quit {
 	echo "Shutting down"
 	kill -9 $(ps aux | grep -v grep | grep surefire | cut -c15-24)
 	end="Aborted during testrun, but ran $run Tests, $fail tests failed, $err tests had errors"
-	curl -X POST -H "Content-type: application/json" --data "{'text':'Morphium integration test just ran: $end'}" https://hooks.slack.com/services/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
+	curl -X POST -H "Content-type: application/json" --data "{'text':'Morphium $version integration test just ran: $end'}" https://hooks.slack.com/services/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
 	exit 1
 }
 
@@ -11,12 +11,14 @@ trap 'quit' ABRT QUIT INT
 
 start=$(date +%s)
 cd $(dirname $0)
+version=$(grep '<version>' pom.xml | head -n1 | tr -d ' a-z<>/')
 
 mvn -Dsurefire.skipAfterFailureCount=2 -Dsurefire.rerunFailingTestsCount=1 test >test.log 2>&1 &
 
 end=""
 while true; do 
 	clear
+	echo "Running tests for version $version"
 	date
 	grep "Running " test.log | tail -n 1
 	a=$(grep "Number: " test.log | tail -n 1); echo "Test number: ${a##*:}"
@@ -66,4 +68,4 @@ let s='(dur-h*3600-m*60)'
 let h=dur/3600; let m='(dur-h*3600)/60';let s='(dur-h*3600-m*60)'; 
 duration=$(printf "Duration: %02d:%02d:%02d" $h $m $s)
 end="$duration - Run $run Tests, $fail tests failed, $err tests had errors"
-curl -X POST -H "Content-type: application/json" --data "{'text':'Morphium integration test just ran: $end'}" https://hooks.slack.com/services/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
+curl -X POST -H "Content-type: application/json" --data "{'text':'Morphium $version integration test just ran: $end'}" https://hooks.slack.com/services/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
