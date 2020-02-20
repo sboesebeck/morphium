94eec33e1d63ee5531aec54585948094f7ca946b
Stephan Bösebeck <stephan@boesebeck.biz>
2020-02-20 16:10:27 +0100

    trying to make tests work again

--- src/de/caluga/morphium/messaging/Msg.java
+++ src/de/caluga/morphium/messaging/Msg.java
@@ -24,7 +24,7 @@ import java.util.*;
 @NoCache
 //timeout <0 - setting relative to replication lag
 //timeout == 0 - wait forever
-@WriteSafety(level = SafetyLevel.WAIT_FOR_SLAVE, waitForJournalCommit = false)
+@WriteSafety(level = SafetyLevel.NORMAL, waitForJournalCommit = false)
 @DefaultReadPreference(ReadPreferenceLevel.PRIMARY)
 @Lifecycle
 @Index({"sender,locked_by,processed_by,recipient,priority,timestamp", "locked_by,processed_by,recipient,priority,timestamp",
