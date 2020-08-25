e855f5d13e911bca46972dd4b0681cff5966e1f7
GermanGeekN <41271949+germangeekn@users.noreply.github.com>
2020-08-25 22:22:29 +0200

    implementing missing aggregation stages

--- src/de/caluga/morphium/aggregation/AggregatorImpl.java
+++ src/de/caluga/morphium/aggregation/AggregatorImpl.java
@@ -7,6 +7,7 @@ import de.caluga.morphium.async.AsyncOperationType;
 import de.caluga.morphium.query.Query;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * User: Stephan Bösebeck
@@ -115,7 +116,16 @@ public class AggregatorImpl<T, R> implements Aggregator<T, R> {
 
     @Override
     public Aggregator<T, R> addFields(Map<String, Object> m) {
-        Map<String, Object> o = Utils.getMap("$addFields", m);
+        Map<String, Object> ret = new LinkedHashMap<>();
+        for (Map.Entry<String, Object> e : m.entrySet()) {
+            if (e.getValue() instanceof Expr) {
+                ret.put(e.getKey(), ((Expr) e.getValue()).toQueryObject());
+            } else {
+                ret.put(e.getKey(), e.getValue());
+            }
+        }
+
+        Map<String, Object> o = Utils.getMap("$addFields", ret);
         params.add(o);
         return this;
     }
@@ -296,59 +306,162 @@ public class AggregatorImpl<T, R> implements Aggregator<T, R> {
     }
 
     @Override
-    public Aggregator<T, R> bucketAuto(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> bucketAuto(Expr groupBy, int numBuckets, Map<String, Expr> output, BucketGranularity granularity) {
+        Map<String, Object> out = new LinkedHashMap<>();
+        for (Map.Entry<String, Expr> e : output.entrySet()) {
+            out.put(e.getKey(), e.getValue().toQueryObject());
+        }
+        params.add(Utils.getMap("$bucketAuto", Utils.getMap("groupBy", groupBy.toQueryObject())
+                .add("buckets", numBuckets)
+                .add("output", out)
+                .add("granularity", granularity.getValue())
+        ));
+
+        return this;
+    }
+
+    //$collStats:
+    //    {
+    //      latencyStats: { histograms: <boolean> },
+    //      storageStats: { scale: <number> },
+    //      count: {},
+    //      queryExecStats: {}
+    //    }
+    @Override
+    public Aggregator<T, R> collStats(Boolean latencyHistograms, Double scale, boolean count, boolean queryExecStats) {
+        Map<String, Object> m = new LinkedHashMap();
+        if (latencyHistograms != null) {
+            m.put("latencyStats", Utils.getMap("histograms", latencyHistograms));
+        }
+        if (scale != null) {
+            m.put("storageStats", Utils.getMap("scale", scale));
+        }
+        if (count) {
+            m.put("count", new HashMap<>());
+        }
+        if (queryExecStats) {
+            m.put("queryExecStats", new HashMap<>());
+        }
+
+        params.add(Utils.getMap("$collStats", m));
+
+        return this;
+    }
+
+
+    //{ $currentOp: { allUsers: <boolean>, idleConnections: <boolean>, idleCursors: <boolean>, idleSessions: <boolean>, localOps: <boolean> } }
+    @Override
+    public Aggregator<T, R> currentOp(boolean allUsers, boolean idleConnections, boolean idleCursors, boolean idleSessions, boolean localOps) {
+        params.add(Utils.getMap("$currentOp", Utils.getMap("allUsers", allUsers)
+                .add("idleConnections", idleConnections)
+                .add("idleCursors", idleCursors)
+                .add("idleSessions", idleSessions)
+                .add("localOps", localOps)
+        ));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> collStats(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> facet(Map<String, Expr> facets) {
+        Map<String, Object> map = Utils.getQueryObjectMap(facets);
+        params.add(Utils.getMap("$facet", map));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> currentOp(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> geoNear(Map<Aggregator.GeoNearFiels, Object> param) {
+        params.add(Utils.getMap("$geoNear", Utils.getNoExprMap((Map) param)));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> facet(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> graphLookup(Class<?> type, Expr startWith, String connectFromField, String connectToField, String as, int maxDepth, String depthField, Query restrictSearchWithMatch) {
+        return graphLookup(morphium.getMapper().getCollectionName(type),
+                startWith,
+                connectFromField,
+                connectToField,
+                as,
+                maxDepth, depthField, restrictSearchWithMatch);
     }
 
     @Override
-    public Aggregator<T, R> geoNear(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> graphLookup(String fromCollection, Expr startWith, String connectFromField, String connectToField, String as, int maxDepth, String depthField, Query restrictSearchWithMatch) {
+        params.add(Utils.getMap("$graphLookup", Utils.getMap("from", (Object) fromCollection)
+                .add("startWith", startWith.toQueryObject())
+                .add("connectFromField", connectFromField)
+                .add("connectToField", connectToField)
+                .add("maxDepth", maxDepth)
+                .add("depthField", depthField)
+                .add("restrictSearchWithMatch", restrictSearchWithMatch.toQueryObject())
+        ));
+
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> graphLookup(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> indexStats() {
+        params.add(Utils.getMap("$indexStats", new HashMap<>()));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> indexStats(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> listLocalSessionsAllUsers() {
+        params.add(Utils.getMap("$listLocalSessions", Utils.getMap("allUsers", true)));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> listLocalSessions(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> listLocalSessions() {
+        params.add(Utils.getMap("$listLocalSessions", new HashMap<>()));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> listSessions(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> listLocalSessions(List<String> users, List<String> dbs) {
+        List<Map<String, Object>> usersList = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            int j = i;
+            if (j > dbs.size()) {
+                j = dbs.size() - 1;
+            }
+            usersList.add(Utils.getMap(users.get(i), dbs.get(j)));
+        }
+        params.add(Utils.getMap("$listLocalSessions", Utils.getMap("users", usersList)));
+        return this;
     }
+
+    @Override
+    public Aggregator<T, R> listSessionsAllUsers() {
+        params.add(Utils.getMap("$listSessions", Utils.getMap("allUsers", true)));
+        return this;
+    }
+
+    @Override
+    public Aggregator<T, R> listSessions() {
+        params.add(Utils.getMap("$listSessions", new HashMap<>()));
+        return this;
+    }
+
+    @Override
+    public Aggregator<T, R> listSessions(List<String> users, List<String> dbs) {
+        List<Map<String, Object>> usersList = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            int j = i;
+            if (j > dbs.size()) {
+                j = dbs.size() - 1;
+            }
+            usersList.add(Utils.getMap(users.get(i), dbs.get(j)));
+        }
+        params.add(Utils.getMap("$listSessions", Utils.getMap("users", usersList)));
+        return this;
+    }
+
 
     @Override
     public Aggregator<T, R> lookup(Map<String, Object> param) {
         return null;
     }
 
-    @Override
-    public Aggregator<T, R> match(Map<String, Object> param) {
-        return null;
-    }
 
     @Override
     public Aggregator<T, R> merge(Map<String, Object> param) {
@@ -356,67 +469,132 @@ public class AggregatorImpl<T, R> implements Aggregator<T, R> {
     }
 
     @Override
-    public Aggregator<T, R> out(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> out(String collectionName) {
+        params.add(Utils.getMap("$out", Utils.getMap("coll", collectionName)));
+        return this;
+    }
+
+    @Override
+    public Aggregator<T, R> out(String db, String collectionName) {
+        params.add(Utils.getMap("$out", Utils.getMap("coll", collectionName)
+                .add("db", db)
+        ));
+        return this;
     }
 
     @Override
     public Aggregator<T, R> planCacheStats(Map<String, Object> param) {
-        return null;
+        params.add(Utils.getMap("$planCacheStats", new HashMap<>()));
+        return this;
+    }
+
+
+    /**
+     * redact needs to resolve to $$DESCEND, $$PRUNE, or $$KEEP
+     * <p>
+     * System Variable	Description
+     * $$DESCEND	$redact returns the fields at the current document level, excluding embedded documents. To include embedded documents and embedded documents within arrays, apply the $cond expression to the embedded documents to determine access for these embedded documents.
+     * $$PRUNE	$redact excludes all fields at this current document/embedded document level, without further inspection of any of the excluded fields. This applies even if the excluded field contains embedded documents that may have different access levels.
+     * $$KEEP	$redact returns or keeps all fields at this current document/embedded document level, without further inspection of the fields at this level. This applies even if the included field contains embedded documents that may have different access levels.
+     */
+    @Override
+    public Aggregator<T, R> redact(Expr redact) {
+        params.add(Utils.getMap("$redact", redact.toQueryObject()));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> redact(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> replaceRoot(Expr newRoot) {
+        params.add(Utils.getMap("$replaceRoot", Utils.getMap("newRoot", newRoot.toQueryObject())));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> replaceRoot(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> replaceWith(Expr newDoc) {
+        params.add(Utils.getMap("$replaceWith", newDoc.toQueryObject()));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> replaceWith(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> sample(int sampleSize) {
+        params.add(Utils.getMap("$sample", Utils.getMap("size", sampleSize)));
+        return this;
+    }
+
+
+    /**
+     * Adds new fields to documents. $set outputs documents that contain all existing fields from the input documents and newly added fields.
+     * <p>
+     * The $set stage is an alias for $addFields.
+     *
+     * @param param
+     * @return
+     */
+    @Override
+    public Aggregator<T, R> set(Map<String, Expr> param) {
+        Map<String, Object> o = Utils.getMap("$set", Utils.getQueryObjectMap(param));
+        params.add(o);
+        return this;
+    }
+
+    /**
+     * The $sortByCount stage is equivalent to the following $group + $sort sequence:
+     * <p>
+     * <p>
+     * { $group: { _id: <expression>, count: { $sum: 1 } } },
+     * { $sort: { count: -1 } }
+     *
+     * @param sortby
+     * @return
+     */
+    @Override
+    public Aggregator<T, R> sortByCount(Expr sortby) {
+        params.add(Utils.getMap("$sortByCount", sortby.toQueryObject()));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> sample(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> unionWith(String collection) {
+        params.add(Utils.getMap("$unionWith", collection));
+        return this;
     }
 
     @Override
-    public Aggregator<T, R> set(Map<String, Object> param) {
-        return null;
-    }
-
-    @Override
-    public Aggregator<T, R> sortByCount(Map<String, Object> param) {
-        return null;
-    }
-
-    @Override
-    public Aggregator<T, R> unionWith(Map<String, Object> param) {
-        return null;
+    public Aggregator<T, R> unionWith(Aggregator agg) {
+        params.add(Utils.getMap("$unionWith", Utils.getMap("coll", (Object) collectionName)
+                .add("pipeline", agg.toAggregationList())
+        ));
+        return this;
     }
 
     @Override
     public Aggregator<T, R> unset(List<String> field) {
-        return null;
+        params.add(Utils.getMap("$unset", field));
+        return this;
     }
 
     @Override
     public Aggregator<T, R> unset(String... param) {
-        return null;
+        params.add(Utils.getMap("$unset", Arrays.asList(param)));
+        return this;
     }
 
     @Override
     public Aggregator<T, R> unset(Enum... field) {
-        return null;
+        List<String> lst = Arrays.asList(field).stream().map((x) -> x.name()).collect(Collectors.toList());
+        params.add(Utils.getMap("$unset",lst));
+        return this;
     }
 
     @Override
     public Aggregator<T, R> genericStage(String stageName, Object param) {
-        return null;
+        if (param instanceof Expr) {
+            param = ((Expr) param).toQueryObject();
+        }
+        if (!stageName.startsWith("$")) {
+            stageName = "$" + stageName;
+        }
+        params.add(Utils.getMap(stageName,param));
+        return this;
     }
 }
