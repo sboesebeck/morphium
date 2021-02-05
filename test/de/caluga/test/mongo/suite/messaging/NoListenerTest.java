1b1e6c50b2eec3082f5c1f1f31bd5800768ce9cf
Stephan Bösebeck <stephan@boesebeck.biz>
2021-02-05 08:10:18 +0100

    processing messages even if no listener defined, added test for it

---
+++ test/de/caluga/test/mongo/suite/messaging/NoListenerTest.java
@@ -0,0 +1,40 @@
+package de.caluga.test.mongo.suite.messaging;
+
+import de.caluga.morphium.messaging.MessageListener;
+import de.caluga.morphium.messaging.Messaging;
+import de.caluga.morphium.messaging.Msg;
+import de.caluga.test.mongo.suite.MorphiumTestBase;
+import org.junit.Test;
+
+public class NoListenerTest extends MorphiumTestBase {
+
+    @Test
+    public void removeListenerTest() throws Exception {
+        Messaging sender = new Messaging(morphium, 10, false);
+        sender.start();
+
+        Messaging r1 = new Messaging(morphium, 10, false);
+        r1.start();
+
+        MessageListener ml = (msg, m) -> {
+            log.info("Got Message");
+            return null;
+        };
+
+        r1.addListenerForMessageNamed("test_listener", ml);
+
+        Msg m = new Msg("test_listener", "a test", "42");
+        sender.sendMessage(m);
+        Thread.sleep(100);
+        morphium.reread(m);
+
+        assert (m.getProcessedBy().size() == 1);
+        r1.removeListenerForMessageNamed("test_listener", ml);
+        m = new Msg("test_listener", "a test", "42");
+        sender.sendMessage(m);
+        Thread.sleep(100);
+        morphium.reread(m);
+        assert (m.getProcessedBy() == null || m.getProcessedBy().size() == 0);
+
+    }
+}
