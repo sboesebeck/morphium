f7092b04cfc3f99488b05c5cd615260a5e0b3ac7
Stephan Bösebeck <stephan@boesebeck.biz>
2021-06-10 23:08:56 +0200

    basic EXpr parsing

--- test/de/caluga/test/mongo/suite/ExprParsingTests.java
+++ test/de/caluga/test/mongo/suite/ExprParsingTests.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 public class ExprParsingTests {
     private final Logger log = LoggerFactory.getLogger(ExprParsingTests.class);
@@ -25,6 +25,94 @@ public class ExprParsingTests {
 
         qo = new HashMap<>();
         qo.put("$add", Arrays.asList(1, 2, 3));
+
+    }
+
+    @Test
+    public void backAndForthTest() {
+        Expr o = Expr.abs(Expr.intExpr(1));
+        Expr o2 = Expr.parse(o.toQueryObject());
+        assert (o.toQueryObject().equals(o2.toQueryObject()));
+        Map<String, Object> context = Utils.getMap("test", 1);
+        assert (o.evaluate(context).equals(o2.evaluate(context)));
+    }
+
+
+    @Test
+    public void allCallerTest() throws Exception {
+        Method[] declaredMethods = Expr.class.getDeclaredMethods();
+        Arrays.sort(declaredMethods, new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        boolean failed = false;
+        List<String> failedElements = new ArrayList<>();
+        int count = 0;
+        for (Method m : declaredMethods) {
+            if (Modifier.isStatic(m.getModifiers())) {
+                try {
+                    Expr expr1;
+                    Expr expr2;
+                    if (m.getParameterTypes().length == 1 && m.getParameterTypes()[0] == Expr.class) {
+                        count++;
+                        log.info("Method with one param: " + m.getName());
+                        expr1 = (Expr) m.invoke(null, Expr.intExpr(1));
+                        expr2 = Expr.parse(expr1.toQueryObject());
+                    } else if (m.getParameterCount() == 1 && m.getParameterTypes()[0].isArray()) {
+                        count++;
+                        log.info("Method with array param: " + m.getName());
+                        expr1 = (Expr) m.invoke(null, new Object[]{new Expr[]{Expr.intExpr(1)}});
+                        expr2 = Expr.parse(expr1.toQueryObject());
+                    } else if (m.getParameterCount() == 2 && m.getParameterTypes()[0] == Expr.class) {
+                        count++;
+                        log.info("Method with two param: " + m.getName());
+                        expr1 = (Expr) m.invoke(null, Expr.intExpr(1), Expr.intExpr(1));
+                        expr2 = Expr.parse(expr1.toQueryObject());
+                    } else if (m.getParameterCount() == 3 && m.getParameterTypes()[0] == Expr.class) {
+                        count++;
+                        log.info("Method with two param: " + m.getName());
+                        expr1 = (Expr) m.invoke(null, Expr.intExpr(1), Expr.intExpr(1), Expr.intExpr(1));
+                        expr2 = Expr.parse(expr1.toQueryObject());
+                    } else {
+                        continue;
+                    }
+                    if (!expr1.toQueryObject().equals(expr2.toQueryObject())) {
+                        log.error("QueryObjects differ");
+                        failed = true;
+                        failedElements.add(m.getName());
+                    }
+                    HashMap<String, Object> context = new HashMap<>();
+                    if (expr1.evaluate(context) == null && expr2.evaluate(context) != null) {
+                        log.error(m + " failed, evaluate null vs not null");
+                        failed = true;
+                        failedElements.add(m.getName());
+                    } else if (expr1.evaluate(context) == null && expr2.evaluate(context) == null) {
+                        //all good
+                    } else {
+                        if (!expr1.evaluate(context).equals(expr2.evaluate(context))) {
+                            log.error(m + " Failed, evaluate results differ: " + expr1.evaluate(context) + "!=" + expr2.evaluate(context));
+                            failed = true;
+                            failedElements.add(m.getName());
+                        }
+                    }
+
+                } catch (Exception e) {
+                    failed = true;
+                    log.error(m + " failed", e);
+                    failedElements.add(m.getName());
+                }
+            }
+        }
+        if (failed) {
+            log.error("Summary... failed Methods:");
+            for (String m : failedElements) {
+                log.error(".... " + m);
+            }
+            log.error("Total fails: " + failedElements.size() + " of total checks " + count);
+        }
+        assert (!failed);
 
     }
 }
