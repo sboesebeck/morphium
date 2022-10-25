8e593b7ba4eeba902e20a48dcd815f9632b08d94
Stephan Bösebeck <sb@caluga.de>
2022-10-25 18:24:25 +0200

    playing with tests

--- getFailedTests.sh
+++ getFailedTests.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-egrep "] Running |Tests run: " test.log/* | grep -B1 FAILURE | cut -f2 -d']' | grep -v "Tests run: " | sed -e 's/Running //' | grep -v -- '--'
-
+for cls in $(egrep "] Running |Tests run: " test.log/* | grep -B1 FAILURE | cut -f2 -d']' | grep -v "Tests run: " | sed -e 's/Running //' | grep -v -- '--'); do
+  # echo "Getting failures in $cls"
+  if [ ! -e ./target/surefire-reports/TEST-$cls.xml ]; then
+    echo "$cls"
+    continue
+  fi
+	failures=$(xq . ./target/surefire-reports/TEST-"$cls".xml | gron | grep 'testcase\[[1-9]*\].failure\["@type"\]' | cut -f 2 -d '[' | cut -f1 -d ']')
+	for id in $(echo $failures); do
+    # echo "ID: '$id'"
+    # xq . ./target/surefire-reports/TEST-$cls.xml | gron | grep "testcase\\[$id\\]\\[\"@name\"\\]" | cut -f2 -d= | tr -d '"; '
+		for m in $(xq . ./target/surefire-reports/TEST-$cls.xml | gron | grep "testcase\\[$id\\]\\[\"@name\"\\]" | cut -f2 -d= | tr -d '"; '); do
+			echo "$cls#$m"
+		done
+	done
+done
