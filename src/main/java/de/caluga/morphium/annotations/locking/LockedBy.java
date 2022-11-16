a3843904666f821c16c9e44a88d5f25031b6276f
Stephan Bösebeck <sb@caluga.de>
2022-11-16 20:35:31 +0100

    new feature: locking

---
+++ src/main/java/de/caluga/morphium/annotations/locking/LockedBy.java
@@ -0,0 +1,15 @@
+
+package de.caluga.morphium.annotations.locking;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target({FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LockedBy {
+}
+
