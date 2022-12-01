88f4f4cca605bbcf11d2d396fa8c35c73b66c6f3
Stephan Bösebeck <sb@caluga.de>
2022-12-01 11:00:36 +0100

    splitting up tests a bit

--- src/test/java/de/caluga/test/mongo/suite/messaging/PausingUnpausingTests.java
+++ src/test/java/de/caluga/test/mongo/suite/messaging/PausingUnpausingTests.java
@@ -1,15 +1,22 @@
 package de.caluga.test.mongo.suite.messaging;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.jupiter.api.Test;
+
+import de.caluga.morphium.Morphium;
+import de.caluga.morphium.MorphiumConfig;
 import de.caluga.morphium.driver.MorphiumId;
+import de.caluga.morphium.messaging.MessageListener;
 import de.caluga.morphium.messaging.Messaging;
 import de.caluga.morphium.messaging.Msg;
 import de.caluga.test.mongo.suite.base.MorphiumTestBase;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class PausingUnpausingTests extends MorphiumTestBase {
     public boolean gotMessage = false;
@@ -413,81 +420,163 @@ public class PausingUnpausingTests extends MorphiumTestBase {
 
 
     }
+    @Test
+    public void exclusivityPausedUnpausingTest() throws Exception {
+        Messaging sender = new Messaging(morphium, 1000, false);
+        sender.setSenderId("sender");
+        morphium.dropCollection(Msg.class, sender.getCollectionName(), null);
+        Thread.sleep(100);
+        sender.start();
+        MorphiumConfig cfg = MorphiumConfig.fromProperties(morphium.getConfig().asProperties());
+        cfg.setCredentialsEncryptionKey(morphium.getConfig().getCredentialsEncryptionKey());
+        cfg.setCredentialsDecryptionKey(morphium.getConfig().getCredentialsDecryptionKey());
+        cfg.setThreadPoolMessagingMaxSize(10);
+        cfg.setThreadPoolMessagingCoreSize(5);
+        cfg.setThreadPoolAsyncOpMaxSize(10);
+        Morphium morphium2 = new Morphium(cfg);
+
+        Messaging receiver = new Messaging(morphium2, (int) (50 + 100 * Math.random()), true, true, 15);
+        receiver.setSenderId("r1");
+        receiver.start();
+
+        cfg = MorphiumConfig.fromProperties(morphium.getConfig().asProperties());
+        cfg.setCredentialsEncryptionKey(morphium.getConfig().getCredentialsEncryptionKey());
+        cfg.setCredentialsDecryptionKey(morphium.getConfig().getCredentialsDecryptionKey());
+        cfg.setThreadPoolMessagingMaxSize(10);
+        cfg.setThreadPoolMessagingCoreSize(5);
+        cfg.setThreadPoolAsyncOpMaxSize(10);
+        Morphium morphium3 = new Morphium(cfg);
+
+        Messaging receiver2 = new Messaging(morphium3, (int) (50 + 100 * Math.random()), false, false, 15);
+        receiver2.setSenderId("r2");
+        receiver2.start();
+
+        cfg = MorphiumConfig.fromProperties(morphium.getConfig().asProperties());
+        cfg.setCredentialsEncryptionKey(morphium.getConfig().getCredentialsEncryptionKey());
+        cfg.setCredentialsDecryptionKey(morphium.getConfig().getCredentialsDecryptionKey());
+        cfg.setThreadPoolMessagingMaxSize(10);
+        cfg.setThreadPoolMessagingCoreSize(5);
+        cfg.setThreadPoolAsyncOpMaxSize(10);
+        Morphium morphium4 = new Morphium(cfg);
+        Messaging receiver3 = new Messaging(morphium4, (int) (50 + 100 * Math.random()), true, false, 15);
+        receiver3.setSenderId("r3");
+        receiver3.start();
+
+        cfg = MorphiumConfig.fromProperties(morphium.getConfig().asProperties());
+        cfg.setCredentialsEncryptionKey(morphium.getConfig().getCredentialsEncryptionKey());
+        cfg.setCredentialsDecryptionKey(morphium.getConfig().getCredentialsDecryptionKey());
+        cfg.setThreadPoolMessagingMaxSize(10);
+        cfg.setThreadPoolMessagingCoreSize(5);
+        cfg.setThreadPoolAsyncOpMaxSize(10);
+        Morphium morphium5 = new Morphium(cfg);
+        morphium5.getConfig().setThreadPoolMessagingMaxSize(10);
+        morphium5.getConfig().setThreadPoolMessagingCoreSize(5);
+        morphium5.getConfig().setThreadPoolAsyncOpMaxSize(10);
+        Messaging receiver4 = new Messaging(morphium5, (int) (50 + 100 * Math.random()), false, true, 15);
+        receiver4.setSenderId("r4");
+        receiver4.start();
+
+        final AtomicInteger received = new AtomicInteger();
+        final AtomicInteger dups = new AtomicInteger();
+        final Map<String, Long> ids = new ConcurrentHashMap<>();
+        final Map<String, String> recById = new ConcurrentHashMap<>();
+        final Map<String, AtomicInteger> recieveCount = new ConcurrentHashMap<>();
+        Thread.sleep(100);
+        try {
+            MessageListener messageListener = (msg, m) -> {
+                msg.pauseProcessingOfMessagesNamed("m");
+                try {
+                    Thread.sleep((long) (300 * Math.random()));
+                } catch (InterruptedException e) {
+                }
+                //log.info("R1: Incoming message "+m.getValue());
+                received.incrementAndGet();
+                recieveCount.putIfAbsent(msg.getSenderId(), new AtomicInteger());
+                recieveCount.get(msg.getSenderId()).incrementAndGet();
+                if (ids.containsKey(m.getMsgId().toString())) {
+                    if (m.isExclusive()) {
+                        log.error("Duplicate recieved message " + msg.getSenderId() + " " + (System.currentTimeMillis() - ids.get(m.getMsgId().toString())) + "ms ago");
+                        if (recById.get(m.getMsgId().toString()).equals(msg.getSenderId())) {
+                            log.error("--- duplicate was processed before by me!");
+                        } else {
+                            log.error("--- duplicate processed by someone else");
+                        }
+                        dups.incrementAndGet();
+                    }
+                }
+                ids.put(m.getMsgId().toString(), System.currentTimeMillis());
+                recById.put(m.getMsgId().toString(), msg.getSenderId());
+                msg.unpauseProcessingOfMessagesNamed("m");
+                return null;
+            };
+            receiver.addListenerForMessageNamed("m", messageListener);
+            receiver2.addListenerForMessageNamed("m", messageListener);
+            receiver3.addListenerForMessageNamed("m", messageListener);
+            receiver4.addListenerForMessageNamed("m", messageListener);
+            int exclusiveAmount = 50;
+            int broadcastAmount = 100;
+            for (int i = 0; i < exclusiveAmount; i++) {
+                int rec = received.get();
+                long messageCount = receiver.getPendingMessagesCount();
+                if (i % 100 == 0) log.info("Send " + i + " recieved: " + rec + " queue: " + messageCount);
+                Msg m = new Msg("m", "m", "v" + i, 3000000, true);
+                m.setExclusive(true);
+                sender.sendMessage(m);
+            }
+            for (int i = 0; i < broadcastAmount; i++) {
+                int rec = received.get();
+                long messageCount = receiver.getPendingMessagesCount();
+                if (i % 100 == 0) log.info("Send boadcast" + i + " recieved: " + rec + " queue: " + messageCount);
+                Msg m = new Msg("m", "m", "v" + i, 3000000, false);
+                sender.sendMessage(m);
+            }
+
+            while (received.get() != exclusiveAmount + broadcastAmount * 4) {
+                int rec = received.get();
+                long messageCount = sender.getPendingMessagesCount();
+
+                log.info("Send excl: " + exclusiveAmount + "  brodadcast: " + broadcastAmount + " recieved: " + rec + " queue: " + messageCount + " currently processing: " + (exclusiveAmount + broadcastAmount * 4 - rec - messageCount));
+                for (Messaging m : Arrays.asList(receiver, receiver2, receiver3, receiver4)) {
+                    assert (m.getRunningTasks() <= 10) : m.getSenderId() + " runs too many tasks! " + m.getRunningTasks();
+                }
+                assert (dups.get() == 0) : "got duplicate message";
+
+                Thread.sleep(1000);
+            }
+            int rec = received.get();
+            long messageCount = sender.getPendingMessagesCount();
+            log.info("Send " + exclusiveAmount + " recieved: " + rec + " queue: " + messageCount);
+            assert (received.get() == exclusiveAmount + broadcastAmount * 4) : "should have received " + (exclusiveAmount + broadcastAmount * 4) + " but actually got " + received.get();
+
+            for (String id : recieveCount.keySet()) {
+                log.info("Reciever " + id + " message count: " + recieveCount.get(id).get());
+            }
+            log.info("R1 active: " + receiver.getRunningTasks());
+            log.info("R2 active: " + receiver2.getRunningTasks());
+            log.info("R3 active: " + receiver3.getRunningTasks());
+            log.info("R4 active: " + receiver4.getRunningTasks());
+
+            logStats(morphium);
+            logStats(morphium2);
+            logStats(morphium3);
+            logStats(morphium4);
+            logStats(morphium5);
+        } finally {
+
+            sender.terminate();
+            receiver.terminate();
+            receiver2.terminate();
+            receiver3.terminate();
+            receiver4.terminate();
+            morphium2.close();
+            morphium3.close();
+            morphium4.close();
+            morphium5.close();
+        }
+
+    }
 
 
-//    @Test
-//    public void massiveAnswerBigQueueTests() throws Exception {
-//        morphium.dropCollection(Msg.class);
-//        Messaging sender = new Messaging(morphium, 100, false, true, 5);
-//        Messaging receiver1 = new Messaging(morphium, 100, false, true, 5);
-//        Messaging receiver2 = new Messaging(morphium, 100, false, true, 5);
-//        Messaging receiver3 = new Messaging(morphium, 100, false, true, 5);
-//        Messaging receiver4 = new Messaging(morphium, 100, false, true, 5);
-//        sender.start();
-//        receiver1.start();
-//        receiver2.start();
-//        receiver3.start();
-//        receiver4.start();
-//        Thread.sleep(1000);
-//        final AtomicInteger sent = new AtomicInteger(0);
-//        final AtomicInteger answered = new AtomicInteger(0);
-//        MessageListener messageListener = (msg, m) -> {
-//            msg.pauseProcessingOfMessagesNamed(m.getName());
-////            log.info("Incoming request! " + m.getMsgId());
-//            Thread.sleep(200 - (int) (100.0 * Math.random()));
-//            Msg answer = new Msg("answer", "answer", "answer", 240 * 1000);
-//            answer.setMapValue(m.getMapValue());
-//            msg.unpauseProcessingOfMessagesNamed(m.getName());
-//            return answer;
-//        };
-//        receiver1.addListenerForMessageNamed("answer_me", messageListener);
-//        receiver2.addListenerForMessageNamed("answer_me", messageListener);
-//        receiver3.addListenerForMessageNamed("answer_me", messageListener);
-//        receiver4.addListenerForMessageNamed("answer_me", messageListener);
-//
-//        sender.addListenerForMessageNamed("answer", (msg, m) -> {
-////            log.info("Anwer came in: " + m.getValue());
-//            answered.incrementAndGet();
-//            return null;
-//        });
-//        Runtime runtime = Runtime.getRuntime();
-//        long startFree = runtime.freeMemory();
-//        long startTotal = runtime.totalMemory();
-//        long startMax = runtime.maxMemory();
-//        int noMsg = 300;
-//        StringBuilder bld = new StringBuilder();
-//        for (int i = 0; i < noMsg; i++) {
-//            bld.setLength(0);
-//            sent.incrementAndGet();
-//            Msg m = new Msg("answer_me", "answer_me_" + i, "answer_me_" + i, 180 * 1000);
-//            for (int b = 0; b < 20240; b++) {
-//                bld.append("- ultra long text -");
-//            }
-//
-//            m.setMapValue(UtilsMap.of("bigValue", bld.toString()));
-//            m.setExclusive(true);
-//            sender.sendMessage(m);
-//        }
-//
-//        long start = System.currentTimeMillis();
-//        while (sent.get() > answered.get()) {
-//            log.info("Got: " + answered.get() + " of " + sent.get());
-//            log.info("=====> Time passed: " + ((System.currentTimeMillis() - start) / 1000 / 60) + " mins");
-//            logmem(startFree, startTotal, startMax);
-//            Thread.sleep(5000);
-//        }
-//        log.info("Got all answers... after " + (System.currentTimeMillis() - start) + "ms");
-//
-//        while (System.currentTimeMillis() - start < 4 * 60 * 1000) {
-//            log.info("=====> Time passed: " + ((System.currentTimeMillis() - start) / 1000 / 60) + " mins");
-//            logmem(startFree, startTotal, startMax);
-//
-//            Thread.sleep(5000);
-//
-//        }
-//        long diff = logmem(startFree, startTotal, startMax);
-//        assert (diff < 10);
-//    }
 
     private long logmem(long startFree, long startTotal, long startMax) {
         System.gc();
