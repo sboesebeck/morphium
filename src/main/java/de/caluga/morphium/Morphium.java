96a5d46f6147e8a67e01c9757b027d1fdc8bc280
Stephan Bösebeck <sb@caluga.de>
2022-12-09 14:56:30 +0100

    removing bad lock algorithm

--- src/main/java/de/caluga/morphium/Morphium.java
+++ src/main/java/de/caluga/morphium/Morphium.java
@@ -10,9 +10,6 @@ import de.caluga.morphium.aggregation.Expr;
 import de.caluga.morphium.annotations.*;
 import de.caluga.morphium.annotations.caching.Cache;
 import de.caluga.morphium.annotations.lifecycle.*;
-import de.caluga.morphium.annotations.locking.Lockable;
-import de.caluga.morphium.annotations.locking.LockedAt;
-import de.caluga.morphium.annotations.locking.LockedBy;
 import de.caluga.morphium.async.AsyncOperationCallback;
 import de.caluga.morphium.async.AsyncOperationType;
 import de.caluga.morphium.bulk.MorphiumBulkContext;
@@ -1885,7 +1882,8 @@ public class Morphium implements AutoCloseable {
      * @return - entity
      */
     public <T> T reread(T o) {
-        if (o==null) return null;
+        if (o == null) { return null; }
+
         return reread(o, objectMapper.getCollectionName(o.getClass()));
     }
 
@@ -4144,7 +4142,7 @@ public class Morphium implements AutoCloseable {
          //                     .verbose()             // Enable verbose logging
          .enableAnnotationInfo()
          //                             .enableFieldInfo()
-         .enableClassInfo()                               // Scan classes, methods, fields, annotations
+         .enableClassInfo()                                  // Scan classes, methods, fields, annotations
          .scan()) {
             ClassInfoList entities = scanResult.getClassesWithAnnotation(Entity.class.getName());
 
@@ -4197,169 +4195,6 @@ public class Morphium implements AutoCloseable {
         return missingIndicesByClass;
     }
 
-    public <T> Query<T> getOutdatedLocksQuery(Class<? extends T> cls, int ms) {
-        if (!getARHelper().isAnnotationPresentInHierarchy(cls, Lockable.class)) {
-            throw new IllegalArgumentException("Type is not lockable " + cls.getName());
-        }
-
-        Query<T> q = createQueryFor(cls);
-
-        for (String f : getARHelper().getFields(cls, LockedAt.class)) {
-            q.f(f).lt(System.currentTimeMillis() - ms).f(f).gt(0);
-        }
-
-        return q;
-    }
-
-    public int releaseLocksOutdated(Class<?> cls, int ms) {
-        try {
-            UpdateMongoCommand cmd = new UpdateMongoCommand(getDriver().getPrimaryConnection(getWriteConcernForClass(cls)));
-
-            try {
-                var q = getOutdatedLocksQuery(cls, ms);
-                cmd.setDb(getDatabase()).setColl(getMapper().getCollectionName(cls));
-                var updates = Doc.of();
-
-                for (String f : getARHelper().getFields(cls, LockedBy.class)) {
-                    updates.put(f, 1);
-                }
-
-                for (String f : getARHelper().getFields(cls, LockedAt.class)) {
-                    updates.put(f, 1);
-                }
-
-                var count = 0;
-
-                while (true) {
-                    cmd.addUpdate(Doc.of("q", q.toQueryObject(), "u", Doc.of("$unset", updates), "multi", false, "upsert", false));
-                    var result = cmd.execute();
-
-                    if (result.containsKey("errmsg")) {
-                        throw new RuntimeException("Error releasing lock:" + result.get("errmsg"));
-                    }
-
-                    if (Integer.valueOf(0).equals(result.get("nModified"))) {
-                        return count;
-                    }
-
-                    count = count + (Integer)result.get("nModified");
-                }
-            } finally {
-                cmd.releaseConnection();
-            }
-        } catch (Exception e) {
-            log.error("Locking failed", e);
-            return 0;
-        }
-    }
-
-    public <T> T lockEntity(T entity, String lockId) throws MorphiumDriverException {
-        return lockEntity(entity, lockId, 0);
-    }
-    public <T> T lockEntity(T entity, String lockId, int maxLockTimeMs) throws MorphiumDriverException {
-        var q = createQueryFor(entity.getClass());
-        q.f(getARHelper().getIdFieldName(entity.getClass())).eq(getARHelper().getId(entity));
-        q.limit(1);
-        var l = lockEntities(q, lockId, maxLockTimeMs);
-
-        if (l != null && l.size() != 0) {
-            T lock = (T)l.get(0);
-            // try {
-            //     Thread.sleep(100);
-            // } catch (InterruptedException e) {
-            // }
-            // lock=reread(lock);
-            return lock;
-        }
-
-        return null;
-    }
-    //Limit done via query
-    public <T> List<T> lockEntities(Query<T> q, String lockId) throws MorphiumDriverException {
-        return lockEntities(q, lockId, 0);
-    }
-    public <T> List<T> lockEntities(Query<T> q, String lockId, int maxLockTimeMs) throws MorphiumDriverException {
-        if (!getARHelper().isAnnotationPresentInHierarchy(q.getType(), Lockable.class)) {
-            throw new IllegalArgumentException("Type is not lockable " + q.getType().getName());
-        }
-
-        q = q.clone();
-        var tsQ = q.clone();
-        var qRet = q.clone();
-        var fields = getARHelper().getFields(q.getType(), LockedBy.class);
-        var tsFields = getARHelper().getFields(q.getType(), LockedAt.class);
-        UpdateMongoCommand cmd = new UpdateMongoCommand(getDriver().getPrimaryConnection(getWriteConcernForClass(q.getType())));
-        cmd.setDb(getDatabase()).setColl(q.getCollectionName());
-        List<Map<String, Object>> updates = new ArrayList<>();
-
-        try {
-            Map<String, Object> upd = new LinkedHashMap<>();
-
-            for (String f : fields) {
-                //setting all those fields to
-                q.f(f).eq(null);
-                qRet.f(f).eq(lockId);
-                upd.put(f, lockId);
-                //.setUpdates(Arrays.asList(Doc.of("q", Doc.of(find), "u", Doc.of(update), "multi", false, "upsert", false)));
-            }
-
-            for (String f : tsFields) {
-                upd.put(f, System.currentTimeMillis());
-
-                if (maxLockTimeMs > 0) {
-                    tsQ.f(f).lt(System.currentTimeMillis() - maxLockTimeMs);
-                }
-            }
-
-            if (q.getLimit() == 0) {
-                //defaulting to 1
-                q.limit(1);
-            }
-
-            var query = q;
-
-            if (maxLockTimeMs > 0) { query = q.q().or(q, tsQ); }
-
-            for (int i = 0; i < q.getLimit(); i++) {
-                updates.add(Doc.of("q", query.toQueryObject(), "u", Doc.of("$set", upd), "multi", false, "upsert", false));
-            }
-
-            cmd.setUpdates(updates);
-            var res = cmd.execute();
-            return qRet.asList();
-        } finally {
-            cmd.releaseConnection();
-        }
-    }
-
-    public void releaseLock(Object obj) throws MorphiumDriverException {
-        if (!getARHelper().isAnnotationPresentInHierarchy(obj.getClass(), Lockable.class)) {
-            throw new IllegalArgumentException("Type is not lockable " + obj.getClass().getName());
-        }
-
-        var q = createQueryFor(obj.getClass()).f(getARHelper().getIdFieldName(obj)).eq(getARHelper().getId(obj));
-        //must not be cached!!!!!
-        var fields = getARHelper().getFields(q.getType(), LockedBy.class);
-        UpdateMongoCommand cmd = new UpdateMongoCommand(getDriver().getPrimaryConnection(getWriteConcernForClass(q.getType())));
-
-        try {
-            cmd.setDb(getDatabase()).setColl(q.getCollectionName());
-            List<Map<String, Object>> updates = new ArrayList<>();
-            Map<String, Object> upd = new LinkedHashMap<>();
-
-            for (String f : fields) {
-                //setting all those fields to
-                // q.f(f).eq(null);
-                upd.put(f, 1);
-                //.setUpdates(Arrays.asList(Doc.of("q", Doc.of(find), "u", Doc.of(update), "multi", false, "upsert", false)));
-            }
-
-            cmd.setUpdates(Arrays.asList(Doc.of("q", q.toQueryObject(), "u", Doc.of("$unset", upd), "multi", true, "upsert", false)));
-            var res = cmd.execute();
-        } finally {
-            cmd.releaseConnection();
-        }
-    }
 
     @Override
     public String toString() {
