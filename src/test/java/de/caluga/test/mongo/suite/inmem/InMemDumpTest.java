7141a7b2ba1ede243c0a9d3ff1d5e5cb2b81a640
Stephan Bösebeck <sb@caluga.de>
2023-02-01 08:58:30 +0100

    additional inMemDriver tests

--- src/test/java/de/caluga/test/mongo/suite/inmem/InMemDumpTest.java
+++ src/test/java/de/caluga/test/mongo/suite/inmem/InMemDumpTest.java
@@ -1,7 +1,9 @@
 package de.caluga.test.mongo.suite.inmem;
 
+import de.caluga.morphium.Utils;
 import de.caluga.morphium.annotations.Entity;
 import de.caluga.morphium.annotations.Id;
+import de.caluga.morphium.driver.inmem.InMemoryDriver;
 import de.caluga.morphium.objectmapping.MorphiumTypeMapper;
 import de.caluga.test.mongo.suite.base.TestUtils;
 import de.caluga.test.mongo.suite.data.ComplexObject;
@@ -9,7 +11,7 @@ import de.caluga.test.mongo.suite.data.EmbeddedObject;
 import de.caluga.test.mongo.suite.data.UncachedObject;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
-
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +24,6 @@ public class InMemDumpTest extends MorphiumInMemTestBase {
 
     @Test
     public void jsonDumpTest() throws Exception {
-
         MorphiumTypeMapper<ObjectId> mapper = new MorphiumTypeMapper<ObjectId>() {
             @Override
             public Object marshall(ObjectId o) {
@@ -30,39 +31,34 @@ public class InMemDumpTest extends MorphiumInMemTestBase {
                 m.put("value", o.toHexString());
                 m.put("class_name", o.getClass().getName());
                 return m;
-
             }
-
             @Override
             public ObjectId unmarshall(Object d) {
                 return new ObjectId(((Map) d).get("value").toString());
             }
         };
         morphium.getMapper().registerCustomMapperFor(ObjectId.class, mapper);
+
         for (int i = 0; i < 10; i++) {
             UncachedObject e = new UncachedObject();
             e.setCounter(i);
             e.setStrValue("value" + i);
             morphium.store(e);
         }
+
         ExportContainer cnt = new ExportContainer();
         cnt.created = System.currentTimeMillis();
-
-//        cnt.data = ((InMemoryDriver) morphium.getDriver()).getDatabase(morphium.getDriver().listDatabases().get(0));
-//
-//        Map<String, Object> s = morphium.getMapper().serialize(cnt);
-//        System.out.println(Utils.toJsonString(s));
-//
-//        morphium.dropCollection(UncachedObject.class);
-//        ExportContainer ex = morphium.getMapper().deserialize(ExportContainer.class, Utils.toJsonString(s));
-//        assertNotNull(ex);;
-//        ((InMemoryDriver) morphium.getDriver()).setDatabase(morphium.getDriver().listDatabases().get(0), ex.data);
-//
-//        List<UncachedObject> result = morphium.createQueryFor(UncachedObject.class).asList();
-//        assert (result.size() == 10);
-//        assert (result.get(1).getCounter() == 1);
+        cnt.data = ((InMemoryDriver) morphium.getDriver()).getDatabase(morphium.getDriver().listDatabases().get(0));
+        Map<String, Object> s = morphium.getMapper().serialize(cnt);
+        System.out.println(Utils.toJsonString(s));
+        morphium.dropCollection(UncachedObject.class);
+        ExportContainer ex = morphium.getMapper().deserialize(ExportContainer.class, Utils.toJsonString(s));
+        assertNotNull(ex);;
+        ((InMemoryDriver) morphium.getDriver()).setDatabase(morphium.getDriver().listDatabases().get(0), ex.data);
+        List<UncachedObject> result = morphium.createQueryFor(UncachedObject.class).asList();
+        assert(result.size() == 10);
+        assert(result.get(1).getCounter() == 1);
     }
-
 
     @Test
     public void driverDumpTest() throws Exception {
@@ -70,38 +66,53 @@ public class InMemDumpTest extends MorphiumInMemTestBase {
             UncachedObject e = new UncachedObject();
             e.setCounter(i);
             e.setStrValue("value" + i);
-            e.setIntData(new int[]{i, i + 1, i + 2});
+            e.setIntData(new int[] {i, i + 1, i + 2});
             e.setDval(42.00001);
-            e.setBinaryData(new byte[]{1, 2, 3, 4, 5});
+            e.setBinaryData(new byte[] {1, 2, 3, 4, 5});
             morphium.store(e);
-
             ComplexObject o = new ComplexObject();
             o.setEinText("A text " + i);
             o.setEmbed(new EmbeddedObject("emb", "v1", System.currentTimeMillis()));
             o.setRef(e);
             morphium.store(o);
-
-
         }
 
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
-
-//        InMemoryDriver driver = (InMemoryDriver) morphium.getDriver();
-//        driver.dump(morphium, morphium.getDriver().listDatabases().get(0), bout);
-//        log.info("database dump is " + bout.size());
-//
-//        driver.close();
-//        driver.connect();
-//        driver.restore(new ByteArrayInputStream(bout.toByteArray()));
-        assertEquals (100,TestUtils.countUC(morphium));
-        assertEquals(100,morphium.createQueryFor(ComplexObject.class).countAll());
+        InMemoryDriver driver = (InMemoryDriver) morphium.getDriver();
+        driver.dump(morphium, morphium.getDriver().listDatabases().get(0), bout);
+        log.info("database dump is " + bout.size());
+        driver.close();
+        driver.connect();
+        driver.restore(new ByteArrayInputStream(bout.toByteArray()));
+        assertEquals(100, TestUtils.countUC(morphium));
+        assertEquals(100, morphium.createQueryFor(ComplexObject.class).countAll());
 
         for (ComplexObject co : morphium.createQueryFor(ComplexObject.class).asList()) {
-            assertNotNull(co.getEinText());
-            ;
-            assertNotNull(co.getRef());
-            ;
+            assertNotNull(co.getEinText());;
+            assertNotNull(co.getRef());;
         }
+    }
+
+    @Test
+    public void testManualDump() throws Exception {
+        createUncachedObjects(100);
+        Map<Class, List<Object>> dump = new HashMap<>();
+
+        for (String cn : morphium.listCollections()) {
+            Class type = morphium.getMapper().getClassForCollectionName(cn);
+            var entities = morphium.createQueryFor(type).asList();
+            dump.put(type, entities);
+        }
+
+        morphium.dropCollection(UncachedObject.class);
+
+        //data is gone
+        //restore
+        for (var e : dump.entrySet()) {
+            morphium.insert(e.getValue());
+        }
+
+        assertEquals(100,morphium.createQueryFor(UncachedObject.class).countAll());
     }
 
     @Entity
