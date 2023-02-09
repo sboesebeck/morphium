c8512afcbbb722ab750f590de377094563123274
Stephan Bösebeck <sb@caluga.de>
2023-02-10 00:25:08 +0100

    tests

--- src/test/java/de/caluga/test/mongo/suite/messaging/BaseMessagingTests.java
+++ src/test/java/de/caluga/test/mongo/suite/messaging/BaseMessagingTests.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
@@ -80,6 +84,68 @@ public class BaseMessagingTests extends MorphiumTestBase {
         Thread.sleep(2000);
         assertEquals(0, morphium.createQueryFor(Msg.class).setCollectionName("msg_lck").countAll());
         assertEquals(amount * 2, count.get());
+    }
+
+    @Test
+    public void exclusiveMessageTest() throws Exception {
+        Messaging sender = new Messaging(morphium, 100, true, true, 10);
+        sender.setUseChangeStream(true);
+        sender.setSenderId("sender");
+        // sender.start();
+
+        Messaging rec1 = new Messaging(morphium, 100, true, true, 13);
+        rec1.setSenderId("rec1");
+        rec1.setUseChangeStream(true).start();
+
+        Messaging rec2 = new Messaging(morphium, 100, true, true, 13);
+        rec2.setSenderId("rec2");
+        rec2.setUseChangeStream(true).start();
+
+        Map<MorphiumId, Long> counts = new HashMap<>();
+        AtomicInteger errors=new AtomicInteger();
+        Hashtable processing=new Hashtable();
+        MessageListener ml=(m, msg) -> {
+            if (processing.containsKey(msg.getMsgId())){
+                errors.incrementAndGet();
+                log.info(m.getSenderId()+": Is just being processed by "+processing.get(msg.getMsgId()));
+            } else {
+                processing.put(msg.getMsgId(), m.getSenderId());
+            }
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+            }
+            if (counts.containsKey(msg.getMsgId())) {
+                log.error(m.getSenderId()+": Already processeed: " + (System.currentTimeMillis() - counts.get(msg.getMsgId())) + "ms ago");
+                log.error(m.getSenderId()+": Msg: processed_by# "+msg.getProcessedBy().size());
+                var cnt=morphium.createQueryFor(MsgLock.class,m.getLockCollectionName()).f("_id").eq(msg.getMsgId()).get();
+                if (cnt!=null){
+                    log.error(m.getSenderId()+": Locked by: "+cnt.getLockId());
+                }
+                errors.incrementAndGet();
+            } else {
+                counts.put(msg.getMsgId(), System.currentTimeMillis());
+            }
+            processing.remove(msg.getMsgId());
+            return null;
+        };
+        rec1.addListenerForMessageNamed("test", ml);;
+        rec2.addListenerForMessageNamed("test", ml);;
+
+        int amount=54;
+        for (int i = 0; i < amount; i++) {
+            sender.sendMessage(new Msg("test", "msgt", "value", 300000, true));
+        }
+        while (counts.size() < amount) {
+            log.info("Msg rec: " + counts.size());
+            Thread.sleep(500);
+        }
+
+        assertEquals(0,errors.get());;
+        log.info("FINISHED! Msg rec: " + counts.size());
+        sender.terminate();
+        rec1.terminate();
+        rec2.terminate();
     }
 
     @Test
