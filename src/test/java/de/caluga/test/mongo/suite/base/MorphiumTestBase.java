346b0adba3494dc2af26522d8a20128a0efb6f50
Stephan Bösebeck <sb@caluga.de>
2023-03-29 10:03:03 +0200

    settings

--- src/test/java/de/caluga/test/mongo/suite/base/MorphiumTestBase.java
+++ src/test/java/de/caluga/test/mongo/suite/base/MorphiumTestBase.java
@@ -1,5 +1,22 @@
 package de.caluga.test.mongo.suite.base;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import de.caluga.morphium.Morphium;
 import de.caluga.morphium.MorphiumConfig;
 import de.caluga.morphium.ShutdownListener;
@@ -15,25 +32,11 @@ import de.caluga.morphium.query.Query;
 import de.caluga.test.OutputHelper;
 import de.caluga.test.mongo.suite.data.CachedObject;
 import de.caluga.test.mongo.suite.data.UncachedObject;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
-//import de.caluga.morphium.driver.inmem.InMemoryDriver;
-//import de.caluga.morphium.driver.meta.MetaDriver;
-//import de.caluga.morphium.driver.singleconnect.SingleConnectDirectDriver;
-//import de.caluga.morphium.driver.singleconnect.SingleConnectThreaddedDriver;
+// import de.caluga.morphium.driver.inmem.InMemoryDriver;
+// import de.caluga.morphium.driver.meta.MetaDriver;
+// import de.caluga.morphium.driver.singleconnect.SingleConnectDirectDriver;
+// import de.caluga.morphium.driver.singleconnect.SingleConnectThreaddedDriver;
 
 /**
  * User: Stpehan Bösebeck
@@ -56,23 +59,27 @@ public class MorphiumTestBase {
     public static synchronized Properties getProps() {
         if (props == null) {
             props = new Properties();
-        }
-        File f = getFile();
-        if (f.exists()) {
-            try {
-                props.load(new FileReader(f));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            File f = getFile();
+
+            if (f.exists()) {
+                try {
+                    props.load(new FileReader(f));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
+
         return props;
     }
 
     private static File getFile() {
         configDir = new File(System.getProperty("user.home") + "/.config/");
+
         if (!configDir.exists()) {
             configDir.mkdirs();
         }
+
         return new File(System.getProperty("user.home") + "/.config/morphiumtest.cfg");
     }
 
@@ -87,21 +94,22 @@ public class MorphiumTestBase {
         //        morphium.close();
     }
 
-//
-//    public static synchronized List<Morphium> getMorphiums() {
-//        if (morphiums == null) {
-//            morphiums = new ArrayList<>();
-//            morphiums.add(morphiumMongodb);
-//            morphiums.add(morphiumInMemeory);
-//            morphiums.add(morphiumSingleConnect);
-//            morphiums.add(morphiumSingleConnectThreadded);
-//            morphiums.add(morphiumMeta);
-//        }
-//        return morphiums;
-//    }
+    //
+    //    public static synchronized List<Morphium> getMorphiums() {
+    //        if (morphiums == null) {
+    //            morphiums = new ArrayList<>();
+    //            morphiums.add(morphiumMongodb);
+    //            morphiums.add(morphiumInMemeory);
+    //            morphiums.add(morphiumSingleConnect);
+    //            morphiums.add(morphiumSingleConnectThreadded);
+    //            morphiums.add(morphiumMeta);
+    //        }
+    //        return morphiums;
+    //    }
 
     private static void storeProps() {
         File f = getFile();
+
         try {
             getProps().store(new FileWriter(f), "created by morphium test");
         } catch (IOException e) {
@@ -111,14 +119,17 @@ public class MorphiumTestBase {
 
     private void init() {
         log.info("in init!");
+
         if (morphium != null) {
             log.info("Closing morphium");
             morphium.close();
             morphium = null;
         }
+
         if (morphium == null) {
             MorphiumConfig cfg;
             Properties p = getProps();
+
             if (p.getProperty("database") != null) {
                 cfg = MorphiumConfig.fromProperties(p);
                 cfg.setMaxConnections(300);
@@ -150,19 +161,15 @@ public class MorphiumTestBase {
                 cfg.setRetryWaitTimeWriter(1000);
                 cfg.setRetryWaitTimeBufferedWriter(1000);
                 cfg.setHeartbeatFrequency(500);
-
                 cfg.setGlobalCacheValidTime(1000);
                 cfg.setHousekeepingTimeout(500);
                 cfg.setThreadPoolMessagingCoreSize(50);
                 cfg.setThreadPoolMessagingMaxSize(1500);
                 cfg.setThreadPoolMessagingKeepAliveTime(10000);
-
                 cfg.setGlobalFsync(false);
                 cfg.setGlobalJ(false);
                 cfg.setGlobalW(1);
-
                 cfg.setCheckForNew(true);
-
                 //            cfg.setMongoAdminUser("adm");
                 //            cfg.setMongoAdminPwd("adm");
                 ////
@@ -170,23 +177,24 @@ public class MorphiumTestBase {
                 //            cfg.setMongoPassword("tst");
                 //            cfg.setMongoLogin("morphium");
                 //            cfg.setMongoPassword("tst");
-
                 //necessary for Replicaset Status to work
                 //            cfg.setMongoAdminUser("admin");
                 //            cfg.setMongoAdminPwd("admin");
-
                 cfg.setDefaultReadPreference(ReadPreference.nearest());
                 p.putAll(cfg.asProperties());
                 p.put("failovertest", "false");
                 cfg.setThreadConnectionMultiplier(2);
                 storeProps();
             }
+
             if (!cfg.isAtlas()) {
                 cfg.setDatabase(cfg.getDatabase() + "_" + number.get());
             }
+
             morphium = new Morphium(cfg);
             log.info("Morphium instanciated");
         }
+
         int num = number.incrementAndGet();
         OutputHelper.figletOutput(log, "---------");
         OutputHelper.figletOutput(log, "Test#: " + num);
@@ -209,40 +217,49 @@ public class MorphiumTestBase {
 
     @AfterEach
     public void tearDown() throws Exception {
-        if (morphium == null) return;
+        if (morphium == null) {
+            return;
+        }
+
         logStats(morphium);
         morphium.getCache().resetCache();
         morphium.resetStatistics();
         //looking for registered shutdownListeners
         List<ShutdownListener> toRemove = new ArrayList<>();
+
         for (ShutdownListener l : morphium.getShutDownListeners()) {
             if (l instanceof Messaging) {
                 ((Messaging) l).terminate();
-                long start=System.currentTimeMillis();
-                var id=((Messaging) l).getSenderId();
+                long start = System.currentTimeMillis();
+                var id = ((Messaging) l).getSenderId();
                 log.info("Terminating still running messaging..." + id);
+
                 while (((Messaging) l).isRunning()) {
                     log.info("Waiting for messaging to finish");
+
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         //swallow
                     }
-                    if (System.currentTimeMillis()-start > 5000){
-                        throw new RuntimeException("Could not kill Messaging: "+id);
+
+                    if (System.currentTimeMillis() - start > 5000) {
+                        throw new RuntimeException("Could not kill Messaging: " + id);
                     }
                 }
+
                 toRemove.add(l);
                 morphium.dropCollection(Msg.class, ((Messaging) l).getCollectionName(), null);
-
             } else if (l instanceof ChangeStreamMonitor) {
                 try {
                     log.info("Changestream Monitor still running");
                     ((ChangeStreamMonitor) l).terminate();
+
                     while (((ChangeStreamMonitor) l).isRunning()) {
                         log.info("Waiting for changestreamMonitor to finish");
                         Thread.sleep(100);
                     }
+
                     Field f = l.getClass().getDeclaredField("listeners");
                     f.setAccessible(true);
                     ((Collection) f.get(l)).clear();
@@ -252,44 +269,55 @@ public class MorphiumTestBase {
                 }
             }
         }
+
         for (ShutdownListener t : toRemove) {
             morphium.removeShutdownListener(t);
         }
+
         MongoConnection con = null;
+
         try {
-            boolean retry=true;
+            boolean retry = true;
 
             while (retry) {
                 con = morphium.getDriver().getPrimaryConnection(null);
                 ListCollectionsCommand cmd = new ListCollectionsCommand(con).setDb(morphium.getDatabase());
+
                 for (var collMap : cmd.execute()) {
                     String coll = (String) collMap.get("name");
                     log.info("Dropping collection " + coll);
+
                     try {
                         morphium.clearCollection(UncachedObject.class, coll); //faking it
                     } catch (Exception e) {
                         //e.printStackTrace();
                     }
+
                     morphium.dropCollection(UncachedObject.class, coll, null); //faking it a bit ;-)
                 }
-                long start=System.currentTimeMillis();
-                retry=false;
+
+                long start = System.currentTimeMillis();
+                retry = false;
+
                 while (cmd.execute().size() > 0) {
                     Thread.sleep(100);
+
                     for (var k : cmd.execute()) {
                         log.info("Collections still there..." + k.get("name"));
                     }
-                    if (System.currentTimeMillis()-start>1500){
-                        retry=true;
+
+                    if (System.currentTimeMillis() - start > 1500) {
+                        retry = true;
                         break;
                     }
                 }
-
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-            if (con != null) con.release();
+            if (con != null) {
+                con.release();
+            }
         }
 
         Thread.sleep(150);
@@ -301,18 +329,21 @@ public class MorphiumTestBase {
 
     public boolean waitForAsyncOperationsToStart(Morphium morphium, long maxWaitMs) {
         long start = System.currentTimeMillis();
+
         while (morphium.getWriteBufferCount() == 0) {
             Thread.yield();
+
             if (System.currentTimeMillis() - start > maxWaitMs) {
                 return false;
             }
         }
+
         return true;
     }
-//
-//    public void createUncachedObjectsInMemory(int amount) {
-//        createUncachedObjects(morphiumInMemeory, amount);
-//    }
+    //
+    //    public void createUncachedObjectsInMemory(int amount) {
+    //        createUncachedObjects(morphiumInMemeory, amount);
+    //    }
 
     public void createUncachedObjects(int amount) {
         createUncachedObjects(morphium, amount);
@@ -320,20 +351,25 @@ public class MorphiumTestBase {
 
     public void createUncachedObjects(Morphium morphium, int amount) {
         List<UncachedObject> lst = new ArrayList<>();
+
         for (int i = 0; i < amount; i++) {
             UncachedObject uc = new UncachedObject();
             uc.setCounter(i + 1);
             uc.setStrValue("v");
             lst.add(uc);
+
             if (i % 1000 == 999) {
                 morphium.insert(lst);
                 lst.clear();
             }
         }
+
         morphium.storeList(lst);
         Query<UncachedObject> q = morphium.createQueryFor(UncachedObject.class);
+
         while (q.countAll() < amount) {
             log.info("Waiting for data to be stored..." + q.countAll() + "/" + amount);
+
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
@@ -347,13 +383,16 @@ public class MorphiumTestBase {
 
     public void createCachedObjects(Morphium morphium, int amount) {
         List<CachedObject> lst = new ArrayList<>();
+
         for (int i = 0; i < amount; i++) {
             CachedObject uc = new CachedObject();
             uc.setCounter(i + 1);
             uc.setValue("v");
             lst.add(uc);
         }
+
         morphium.storeList(lst);
+
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
@@ -369,25 +408,40 @@ public class MorphiumTestBase {
     public void logStats(Morphium m) {
         Map<String, Double> stats = m.getStatistics();
         log.info("Statistics: ");
+
         for (Map.Entry<String, Double> e : stats.entrySet()) {
             log.info(e.getKey() + " - " + e.getValue());
         }
     }
 
     public void waitForWrites() {
-        TestUtils.waitForWrites(morphium,log);
+        TestUtils.waitForWrites(morphium, log);
     }
 
     public static boolean stringWordCompare(String m1, String m2) {
-        if (m1 == null && m2 == null) return true;
-        if (m1 == null || m2 == null) return false;
+        if (m1 == null && m2 == null) {
+            return true;
+        }
+
+        if (m1 == null || m2 == null) {
+            return false;
+        }
+
         m1 = m1.replaceAll(" ", "");
         m2 = m2.replaceAll(" ", "");
-        if (m1.length() != m2.length()) return false;
-        String[] wrd = m1.split("[ \\{\\},\\.\\(\\)\\[\\]]");
-        for (String w : wrd) {
-            if (!m2.contains(w)) return false;
+
+        if (m1.length() != m2.length()) {
+            return false;
         }
+
+        String[] wrd = m1.split("[ \\{\\},\\.\\(\\)\\[\\]]");
+
+        for (String w : wrd) {
+            if (!m2.contains(w)) {
+                return false;
+            }
+        }
+
         return true;
     }
 
