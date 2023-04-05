9d7db555bd2d3ff1afa84b7449974bc9a2ffc073
Stephan Bösebeck <sb@caluga.de>
2023-04-05 13:12:07 +0200

    avoiding problems terminating messagings

--- src/test/java/de/caluga/test/mongo/suite/messaging/SpeedTests.java
+++ src/test/java/de/caluga/test/mongo/suite/messaging/SpeedTests.java
@@ -17,6 +17,7 @@ public class SpeedTests extends MultiDriverTestBase {
     public void writeSpeed(Morphium morphium) throws Exception {
         String tstName = new Object() {} .getClass().getEnclosingMethod().getName();
         log.info("Running test " + tstName + " with " + morphium.getDriver().getName());
+
         try (morphium) {
             morphium.clearCollection(Msg.class);
             Messaging msg = new Messaging(morphium, 100, false, true, 10);
@@ -25,24 +26,32 @@ public class SpeedTests extends MultiDriverTestBase {
             try {
                 final long dur = 1000;
                 final long start = System.currentTimeMillis();
+
                 for (int i = 0; i < 25; i++) {
                     new Thread() {
                         public void run() {
                             Msg m = new Msg("test", "test", "testval", 30000);
+
                             while (System.currentTimeMillis() < start + dur) {
                                 msg.sendMessage(m);
                                 m.setMsgId(null);
                             }
                         }
-                    }.start();
+                    } .start();
                 }
+
                 while (System.currentTimeMillis() < start + dur) {
                     Thread.sleep(10);
                 }
+
                 long cnt = morphium.createQueryFor(Msg.class).countAll();
                 log.info("stored msg: " + cnt + " in " + dur + "ms");
             } finally {
-                msg.terminate();
+                try {
+                    msg.terminate();
+                } catch (Exception e) {
+                    log.warn("Messageing termination failed!");
+                }
             }
         }
     }
@@ -52,48 +61,53 @@ public class SpeedTests extends MultiDriverTestBase {
     public void writeRecSpeed(Morphium morphium) throws Exception {
         String tstName = new Object() {} .getClass().getEnclosingMethod().getName();
         log.info("Running test " + tstName + " with " + morphium.getDriver().getName());
+
         try (morphium) {
             morphium.clearCollection(Msg.class);
-//        morphium.getConfig().setThreadPoolAsyncOpCoreSize(1000);
+            //        morphium.getConfig().setThreadPoolAsyncOpCoreSize(1000);
             Messaging sender = new Messaging(morphium, 100, false, true, 10);
             sender.start();
             Messaging receiver = new Messaging(morphium, 100, true, true, 100);
             receiver.start();
+
             try {
                 final AtomicInteger recCount = new AtomicInteger();
-
                 receiver.addMessageListener(new MessageListener() {
                     @Override
-                    public Msg onMessage(Messaging msg, Msg m)  {
+                    public Msg onMessage(Messaging msg, Msg m) {
                         recCount.incrementAndGet();
                         return null;
                     }
                 });
-
                 final long dur = 1000;
-
                 final long start = System.currentTimeMillis();
 
                 for (int i = 0; i < 15; i++) {
                     new Thread() {
                         public void run() {
                             Msg m = new Msg("test", "test", "testval", 30000);
+
                             while (System.currentTimeMillis() < start + dur) {
                                 sender.sendMessage(m);
                                 m.setMsgId(null);
                             }
                         }
-                    }.start();
+                    } .start();
                 }
 
                 while (System.currentTimeMillis() < start + dur) {
                     Thread.sleep(10);
                 }
+
                 long cnt = morphium.createQueryFor(Msg.class).countAll();
                 log.info("Messages sent: " + cnt + " received: " + recCount.get() + " in " + dur + "ms");
             } finally {
-                sender.terminate();
-                receiver.terminate();
+                try {
+                    sender.terminate();
+                    receiver.terminate();
+                } catch (Exception e) {
+                    log.warn("Messageing termination failed!");
+                }
             }
         }
     }
@@ -103,8 +117,9 @@ public class SpeedTests extends MultiDriverTestBase {
     public void writeExclusiveRec(Morphium morphium) throws Exception {
         String tstName = new Object() {} .getClass().getEnclosingMethod().getName();
         log.info("Running test " + tstName + " with " + morphium.getDriver().getName());
+
         try (morphium) {
-//        morphium.getConfig().setThreadPoolAsyncOpCoreSize(1000);
+            //        morphium.getConfig().setThreadPoolAsyncOpCoreSize(1000);
             morphium.clearCollection(Msg.class);
             Messaging sender = new Messaging(morphium, 100, false, true, 10);
             sender.start();
@@ -112,9 +127,9 @@ public class SpeedTests extends MultiDriverTestBase {
             receiver.start();
             Messaging receiver2 = new Messaging(morphium, 100, true, true, 100);
             receiver2.start();
+
             try {
                 final AtomicInteger recCount = new AtomicInteger();
-
                 receiver.addMessageListener((msg, m) -> {
                     recCount.incrementAndGet();
                     return null;
@@ -123,9 +138,7 @@ public class SpeedTests extends MultiDriverTestBase {
                     recCount.incrementAndGet();
                     return null;
                 });
-
                 final long dur = 1000;
-
                 final long start = System.currentTimeMillis();
 
                 for (int i = 0; i < 15; i++) {
@@ -133,23 +146,29 @@ public class SpeedTests extends MultiDriverTestBase {
                         public void run() {
                             Msg m = new Msg("test", "test", "testval", 30000);
                             m.setExclusive(true);
+
                             while (System.currentTimeMillis() < start + dur) {
                                 sender.sendMessage(m);
                                 m.setMsgId(null);
                             }
                         }
-                    }.start();
+                    } .start();
                 }
 
                 while (System.currentTimeMillis() < start + dur) {
                     Thread.sleep(10);
                 }
+
                 long cnt = morphium.createQueryFor(Msg.class).countAll();
                 log.info("Messages sent: " + cnt + " received: " + recCount.get() + " in " + dur + "ms");
             } finally {
-                sender.terminate();
-                receiver.terminate();
-                receiver2.terminate();
+                try {
+                    sender.terminate();
+                    receiver.terminate();
+                    receiver2.terminate();
+                } catch (Exception e) {
+                    log.warn("Messageing termination failed!");
+                }
             }
         }
     }
