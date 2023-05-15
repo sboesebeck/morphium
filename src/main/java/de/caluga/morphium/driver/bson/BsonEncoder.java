d1e31d379f70554fdb913192c0b475412eefad95
Stephan Bösebeck <sb@caluga.de>
2023-05-15 13:50:25 +0200

    moving Enum out of BsonEncode
    it seems like this is reducing memory footprint

--- src/main/java/de/caluga/morphium/driver/bson/BsonEncoder.java
+++ src/main/java/de/caluga/morphium/driver/bson/BsonEncoder.java
@@ -361,14 +361,4 @@ public class BsonEncoder {
         return this;
     }
 
-
-    public enum UUIDRepresentation {
-        UNSPECIFIED(-1), STANDARD(4), C_SHARP_LEGACY(3), JAVA_LEGACY(3), PYTHON_LEGACY(3);
-
-        int subtype;
-
-        UUIDRepresentation(int s) {
-            subtype = s;
-        }
-    }
 }
