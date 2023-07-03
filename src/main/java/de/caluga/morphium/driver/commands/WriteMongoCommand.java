766c28dbfdf2d3da571ae458c1380b620f716477
Stephan Bösebeck <sb@caluga.de>
2023-07-03 10:47:35 +0200

    failover fix - not primary caused too many errors

--- src/main/java/de/caluga/morphium/driver/commands/WriteMongoCommand.java
+++ src/main/java/de/caluga/morphium/driver/commands/WriteMongoCommand.java
@@ -1,12 +1,12 @@
 package de.caluga.morphium.driver.commands;
-
 import de.caluga.morphium.driver.Doc;
 import de.caluga.morphium.driver.MorphiumCursor;
-import de.caluga.morphium.driver.MorphiumDriver;
 import de.caluga.morphium.driver.MorphiumDriverException;
 import de.caluga.morphium.driver.wire.MongoConnection;
 
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class WriteMongoCommand<T extends MongoCommand> extends MongoCommand<T> {
     private Map<String, Object> writeConcern;
@@ -35,17 +35,43 @@ public abstract class WriteMongoCommand<T extends MongoCommand> extends MongoCom
     }
 
     public Map<String, Object> execute() throws MorphiumDriverException {
-        if (!getConnection().isConnected()) throw new MorphiumDriverException("Not connected");
+        if (!getConnection().isConnected()) {
+            throw new MorphiumDriverException("Not connected");
+        }
+
         MongoConnection con = getConnection();
-
         //noinspection unchecked
-
         setMetaData("server", con.getConnectedTo());
         long start = System.currentTimeMillis();
         int msg = con.sendCommand(this);
-        var crs = con.readSingleAnswer(msg);
-        long dur = System.currentTimeMillis() - start;
-        setMetaData("duration", dur);
-        return crs;
+
+        try {
+            var crs = con.readSingleAnswer(msg);
+            long dur = System.currentTimeMillis() - start;
+            setMetaData("duration", dur);
+            return crs;
+        } catch (MorphiumDriverException e) {
+            if (e.getMessage().equals("not primary")) {
+
+                Logger log=LoggerFactory.getLogger(WriteMongoCommand.class);
+                var drv=getConnection().getDriver();
+
+                drv.releaseConnection(getConnection());
+                log.warn("node no primary anymore - waiting for failover");
+                try {
+                    Thread.sleep(drv.getHeartbeatFrequency()*2);
+                } catch (InterruptedException e1) {
+                    //swallow
+                }
+                log.warn("Retrying... (recursive)");
+                setConnection(drv.getPrimaryConnection(null));
+                //retrying
+                return execute();
+
+            } else {
+                throw e;
+            }
+        }
+
     }
 }
