4d0197adf77765607e0411fda435092ce817b150
Stephan Bösebeck <sb@caluga.de>
2024-05-14 10:27:39 +0200

    code improvement, use touch

--- src/main/java/de/caluga/morphium/driver/wire/MongoConnection.java
+++ src/main/java/de/caluga/morphium/driver/wire/MongoConnection.java
@@ -1,17 +1,16 @@
 package de.caluga.morphium.driver.wire;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import de.caluga.morphium.driver.MorphiumCursor;
 import de.caluga.morphium.driver.MorphiumDriver;
 import de.caluga.morphium.driver.MorphiumDriverException;
 import de.caluga.morphium.driver.commands.MongoCommand;
 import de.caluga.morphium.driver.commands.WatchCommand;
-import de.caluga.morphium.driver.commands.result.SingleElementResult;
 import de.caluga.morphium.driver.wireprotocol.OpMsg;
-
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public interface MongoConnection extends Closeable {
     void setCredentials(String authDb, String userName, String password);
@@ -44,9 +43,9 @@ public interface MongoConnection extends Closeable {
     // OpMsg getReplyFor(int msgid, long timeout) throws MorphiumDriverException;
     OpMsg readNextMessage(int timeout) throws MorphiumDriverException;
 
-//    void sendQuery(OpMsg q) throws MorphiumDriverException;
+    //    void sendQuery(OpMsg q) throws MorphiumDriverException;
 
-//    OpMsg sendAndWaitForReply(OpMsg q) throws MorphiumDriverException;
+    //    OpMsg sendAndWaitForReply(OpMsg q) throws MorphiumDriverException;
 
     Map<String, Object> readSingleAnswer(int id) throws MorphiumDriverException;
 
@@ -58,10 +57,10 @@ public interface MongoConnection extends Closeable {
 
     List<Map<String, Object>> readAnswerFor(MorphiumCursor crs) throws MorphiumDriverException;
 
-//    Map<String, Object> getSingleDocAndKillCursor(OpMsg msg) throws MorphiumDriverException;
+    //    Map<String, Object> getSingleDocAndKillCursor(OpMsg msg) throws MorphiumDriverException;
 
-//    List<Map<String, Object>> readBatches(int waitingfor, int batchSize) throws MorphiumDriverException;
+    //    List<Map<String, Object>> readBatches(int waitingfor, int batchSize) throws MorphiumDriverException;
 
-//    int sendCommand(Map<String, Object> cmd) throws MorphiumDriverException;
+    //    int sendCommand(Map<String, Object> cmd) throws MorphiumDriverException;
     int sendCommand(MongoCommand cmd) throws MorphiumDriverException;
 }
