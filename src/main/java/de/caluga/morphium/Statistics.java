ec258b18bbc30583aeb99cb1aadfb8ac286c9788
Stephan Bösebeck <sb@caluga.de>
2024-05-22 08:46:17 +0200

    making it more robust

--- src/main/java/de/caluga/morphium/Statistics.java
+++ src/main/java/de/caluga/morphium/Statistics.java
@@ -29,13 +29,13 @@ public class Statistics extends HashMap<String, Double> {
             }
 
             super.put(StatisticKeys.CACHE_ENTRIES.name(), entries);
+            super.put(StatisticKeys.CHITSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CHITS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
+                StatisticKeys.CMISS).get()) * 100.0);
+            super.put(StatisticKeys.CMISSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CMISS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
+                StatisticKeys.CMISS).get()) * 100.0);
         }
 
         super.put(StatisticKeys.WRITE_BUFFER_ENTRIES.name(), (double) morphium.getWriteBufferCount());
-        super.put(StatisticKeys.CHITSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CHITS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
-            StatisticKeys.CMISS).get()) * 100.0);
-        super.put(StatisticKeys.CMISSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CMISS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
-            StatisticKeys.CMISS).get()) * 100.0);
     }
 
 
