9a48a8b2bf09adb590daa8d5e43b1c1798d18c34
Stephan Bösebeck <sb@caluga.de>
2024-05-22 08:27:07 +0200

    making status info more robust

--- src/main/java/de/caluga/morphium/Statistics.java
+++ src/main/java/de/caluga/morphium/Statistics.java
@@ -13,23 +13,29 @@ public class Statistics extends HashMap<String, Double> {
     @SuppressWarnings("rawtypes")
     public Statistics(Morphium morphium) {
         this.morphium = morphium;
+
         for (Map.Entry<StatisticKeys, StatisticValue> et : morphium.getStats().entrySet()) {
             super.put(et.getKey().name(), (double) et.getValue().get());
         }
+
         double entries = 0;
 
-        Map<String, Integer> map = morphium.getCache().getSizes();
-        for (Map.Entry<String, Integer> e : map.entrySet()) {
-            super.put("X-Entries for: " + e.getKey(), (double) e.getValue());
-            entries += e.getValue();
+        if (morphium.getCache() != null) {
+            Map<String, Integer> map = morphium.getCache().getSizes();
+
+            for (Map.Entry<String, Integer> e : map.entrySet()) {
+                super.put("X-Entries for: " + e.getKey(), (double) e.getValue());
+                entries += e.getValue();
+            }
+
+            super.put(StatisticKeys.CACHE_ENTRIES.name(), entries);
         }
 
-        super.put(StatisticKeys.CACHE_ENTRIES.name(), entries);
-
-
         super.put(StatisticKeys.WRITE_BUFFER_ENTRIES.name(), (double) morphium.getWriteBufferCount());
-        super.put(StatisticKeys.CHITSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CHITS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(StatisticKeys.CMISS).get()) * 100.0);
-        super.put(StatisticKeys.CMISSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CMISS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(StatisticKeys.CMISS).get()) * 100.0);
+        super.put(StatisticKeys.CHITSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CHITS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
+            StatisticKeys.CMISS).get()) * 100.0);
+        super.put(StatisticKeys.CMISSPERC.name(), ((double) morphium.getStats().get(StatisticKeys.CMISS).get()) / (morphium.getStats().get(StatisticKeys.CHITS).get() + morphium.getStats().get(
+            StatisticKeys.CMISS).get()) * 100.0);
     }
 
 
@@ -70,6 +76,7 @@ public class Statistics extends HashMap<String, Double> {
         StringBuilder b = new StringBuilder();
         StatisticKeys[] lst = morphium.getStats().keySet().toArray(new StatisticKeys[0]);
         Arrays.sort(lst);
+
         for (StatisticKeys k : lst) {
             b.append("- ");
             b.append(k.toString());
@@ -77,6 +84,7 @@ public class Statistics extends HashMap<String, Double> {
             b.append(morphium.getStats().get(k));
             b.append("\n");
         }
+
         return b.toString();
     }
 }
