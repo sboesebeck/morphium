6a1f0e539cde4bd79b1c1de4ba5ee8a8485e98ca
Stephan Bösebeck <sb@caluga.de>
2025-02-11 16:31:45 +0100

    timings in tests

--- src/test/java/de/caluga/test/mongo/suite/base/MapListTest.java
+++ src/test/java/de/caluga/test/mongo/suite/base/MapListTest.java
@@ -27,9 +27,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class MapListTest extends MorphiumTestBase {
     @Test
-    public void mapListTest() {
+    public void mapListTest() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         MapListObject o = new MapListObject();
         Map<String, Object> map = new HashMap<>();
         map.put("a_string", "This is a string");
@@ -39,14 +38,12 @@ public class MapListTest extends MorphiumTestBase {
         map.put("Entity", new UncachedObject());
         o.setMapValue(map);
         o.setName("A map-value");
-
-        Map<String, List<Integer>> listMap = new HashMap<>();
+        Map<String, List<Integer >> listMap = new HashMap<>();
         List<Integer> lst = new ArrayList<>();
         lst.add(1);
         lst.add(5);
         lst.add(3);
         listMap.put("eins-fuenf-drei", lst);
-
         lst = new ArrayList<>();
         lst.add(200);
         lst.add(300);
@@ -55,16 +52,15 @@ public class MapListTest extends MorphiumTestBase {
         listMap.put("zweihundert", lst);
         o.setMapListValue(listMap);
         morphium.store(o);
-
+        Thread.sleep(100);
         MapListObject ml = morphium.findById(MapListObject.class, o.getId());
-        assert (ml.getMapListValue().get("eins-fuenf-drei").size() == 3);
-        assert (ml.getMapListValue().get("zweihundert").size() == 4);
+        assert(ml.getMapListValue().get("eins-fuenf-drei").size() == 3);
+        assert(ml.getMapListValue().get("zweihundert").size() == 4);
     }
 
     @Test
-    public void mapListEmbTest() {
+    public void mapListEmbTest() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
         Map<String, Object> map = new HashMap<>();
         map.put("a_string", "This is a string");
@@ -74,14 +70,12 @@ public class MapListTest extends MorphiumTestBase {
         map.put("Entity", new UncachedObject());
         o.setMapValue(map);
         o.setName("A map-value");
-
-        Map<String, List<Integer>> listMap = new HashMap<>();
+        Map<String, List<Integer >> listMap = new HashMap<>();
         List<Integer> lst = new ArrayList<>();
         lst.add(1);
         lst.add(5);
         lst.add(3);
         listMap.put("eins-fuenf-drei", lst);
-
         lst = new ArrayList<>();
         lst.add(200);
         lst.add(300);
@@ -89,13 +83,11 @@ public class MapListTest extends MorphiumTestBase {
         lst.add(1);
         listMap.put("zweihundert", lst);
         o.setMapListValue(listMap);
-
         Map<String, EmbObj> map2 = new HashMap<>();
         map2.put("test", new EmbObj("val", 22));
         map2.put("test2", new EmbObj("vvv", 42));
         o.setMap2(map2);
-
-        Map<String, List<EmbObj>> map1 = new HashMap<>();
+        Map<String, List<EmbObj >> map1 = new HashMap<>();
         List<EmbObj> objLst = new ArrayList<>();
         objLst.add(new EmbObj("in list 1", 7331));
         objLst.add(new EmbObj("in list 2", 57));
@@ -106,29 +98,25 @@ public class MapListTest extends MorphiumTestBase {
         objLst.add(new EmbObj("in list again 2", 527));
         objLst.add(new EmbObj("in list again too", 421));
         map1.put("2nd", objLst);
-
         o.setMap1(map1);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
         assertNotNull(ml, "Not Found?!?!?!?");
-        assert (ml.getMapListValue().get("eins-fuenf-drei").size() == 3);
-        assert (ml.getMapListValue().get("zweihundert").size() == 4);
+        assert(ml.getMapListValue().get("eins-fuenf-drei").size() == 3);
+        assert(ml.getMapListValue().get("zweihundert").size() == 4);
         assertNotNull(ml.getMapListValue().get("zweihundert").get(0));
         ;
-
         assertNotNull(ml.getMap1().get("2nd").get(0).getTest());
         ;
-        assert (ml.getMap2().get("test").getTest().equals("val"));
+        assert(ml.getMap2().get("test").getTest().equals("val"));
     }
 
     @Test
-    public void testComplexList() {
+    public void testComplexList() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        List<Map<String, String>> lst = new ArrayList<>();
+        List<Map<String, String >> lst = new ArrayList<>();
         Map<String, String> strMap = new HashMap<>();
         strMap.put("tst1", "bla");
         strMap.put("tst2", "fasel");
@@ -138,81 +126,72 @@ public class MapListTest extends MorphiumTestBase {
         strMap.put("tst2-2", "blub");
         strMap.put("tst2-3", "peng");
         lst.add(strMap);
-
         o.setMap7(lst);
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
         assertNotNull(ml, "Not Found?!?!?!?");
-        assert (ml.getMap7().get(0).get("tst1").equals("bla"));
-        assert (ml.getMap7().get(1).get("tst2-2").equals("blub"));
+        assert(ml.getMap7().get(0).get("tst1").equals("bla"));
+        assert(ml.getMap7().get(1).get("tst2-2").equals("blub"));
     }
 
     @Test
-    public void testMapOfListsString() {
+    public void testMapOfListsString() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        Map<String, List<String>> m = new HashMap<>();
+        Map<String, List<String >> m = new HashMap<>();
         List<String> lst = new ArrayList<>();
         lst.add("bla");
         lst.add("fasel");
         m.put("m1", lst);
-
         lst = new ArrayList<>();
         lst.add("foo");
         lst.add("bar");
         lst.add("grin");
         m.put("m2", lst);
         o.setMap3(m);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
-        assert (ml.getMap3().get("m1").get(1).equals("fasel"));
-        assert (ml.getMap3().get("m2").get(2).equals("grin"));
+        assert(ml.getMap3().get("m1").get(1).equals("fasel"));
+        assert(ml.getMap3().get("m2").get(2).equals("grin"));
     }
 
     @Test
-    public void testMapOfListsEmb() {
+    public void testMapOfListsEmb() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        Map<String, List<EmbObj>> m = new HashMap<>();
+        Map<String, List<EmbObj >> m = new HashMap<>();
         List<EmbObj> lst = new ArrayList<>();
         lst.add(new EmbObj("bla", 12));
         lst.add(new EmbObj("fasel", 42));
         m.put("m1", lst);
-
         lst = new ArrayList<>();
         lst.add(new EmbObj("foo", 42452));
         lst.add(new EmbObj("bar", 2));
         lst.add(new EmbObj("grin", 7331));
         m.put("m2", lst);
         o.setMap4(m);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         Query<CMapListObject> q = morphium.createQueryFor(CMapListObject.class).f("id").eq(o.getId());
         q.setReadPreferenceLevel(ReadPreferenceLevel.PRIMARY);
         CMapListObject ml = q.get();
-        assert (ml.getMap4().get("m1").get(1).getTest().equals("fasel"));
-        assert (ml.getMap4().get("m1").get(1).getValue() == 42);
-        assert (ml.getMap4().get("m2").get(2).getTest().equals("grin"));
-        assert (ml.getMap4().get("m2").get(2).getValue() == 7331);
+        assert(ml.getMap4().get("m1").get(1).getTest().equals("fasel"));
+        assert(ml.getMap4().get("m1").get(1).getValue() == 42);
+        assert(ml.getMap4().get("m2").get(2).getTest().equals("grin"));
+        assert(ml.getMap4().get("m2").get(2).getValue() == 7331);
     }
 
     @Test
-    public void testMapOfMaps() {
+    public void testMapOfMaps() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        Map<String, Map<String, String>> m = new HashMap<>();
+        Map<String, Map<String, String >> m = new HashMap<>();
         Map<String, String> mVal = new HashMap<>();
         mVal.put("bla", "fasel");
         mVal.put("foo", "bar");
         m.put("test", mVal);
-
         mVal = new HashMap<>();
         mVal.put("surname", "nachname");
         mVal.put("first name", "vorname");
@@ -220,27 +199,23 @@ public class MapListTest extends MorphiumTestBase {
         mVal.put("foo", "bla");
         mVal.put("bar", "blub");
         m.put("translate", mVal);
-
         o.setMap5(m);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
-        assert (ml.getMap5().get("test").get("bla").equals("fasel"));
-        assert (ml.getMap5().get("translate").get("foo").equals("bla"));
+        assert(ml.getMap5().get("test").get("bla").equals("fasel"));
+        assert(ml.getMap5().get("translate").get("foo").equals("bla"));
     }
 
     @Test
-    public void testMapOfMapEmbObj() {
+    public void testMapOfMapEmbObj() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        Map<String, Map<String, EmbObj>> m = new HashMap<>();
+        Map<String, Map<String, EmbObj >> m = new HashMap<>();
         Map<String, EmbObj> mVal = new HashMap<>();
         mVal.put("bla", new EmbObj("fasel", 1));
         mVal.put("foo", new EmbObj("bar", 2));
         m.put("test", mVal);
-
         mVal = new HashMap<>();
         mVal.put("surname", new EmbObj("nachname", 31));
         mVal.put("first name", new EmbObj("vorname", 42));
@@ -248,23 +223,20 @@ public class MapListTest extends MorphiumTestBase {
         mVal.put("foo", new EmbObj("bla", 123));
         mVal.put("bar", new EmbObj("blub", 1515));
         m.put("translate", mVal);
-
         o.setMap5a(m);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
-        assert (ml.getMap5a().get("test").get("bla").getTest().equals("fasel"));
-        assert (ml.getMap5a().get("translate").get("foo").getTest().equals("bla"));
+        assert(ml.getMap5a().get("test").get("bla").getTest().equals("fasel"));
+        assert(ml.getMap5a().get("translate").get("foo").getTest().equals("bla"));
     }
 
     @Test
-    public void testListOfListOfMap() {
+    public void testListOfListOfMap() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
-
         CMapListObject o = new CMapListObject();
-        List<List<Map<String, String>>> lst = new ArrayList<>();
-        List<Map<String, String>> l2 = new ArrayList<>();
+        List<List<Map<String, String >>> lst = new ArrayList<>();
+        List<Map<String, String >> l2 = new ArrayList<>();
         Map<String, String> map = new HashMap<>();
         map.put("k1", "v1");
         map.put("k2", "v2");
@@ -275,7 +247,6 @@ public class MapListTest extends MorphiumTestBase {
         map.put("k31", "v31");
         l2.add(map);
         lst.add(l2);
-
         l2 = new ArrayList<>();
         map = new HashMap<>();
         map.put("k15", "v1");
@@ -291,22 +262,20 @@ public class MapListTest extends MorphiumTestBase {
         map.put("k514", "v21");
         map.put("k513", "v31");
         l2.add(map);
-
         lst.add(l2);
         o.setMap7a(lst);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
-        assert (ml.getMap7a().get(1).get(0).get("k15").equals("v1"));
+        assert(ml.getMap7a().get(1).get(0).get("k15").equals("v1"));
     }
 
     @Test
-    public void testMapListMapEmb() {
+    public void testMapListMapEmb() throws InterruptedException {
         morphium.dropCollection(MapListObject.class);
         CMapListObject o = new CMapListObject();
-        Map<String, List<Map<String, EmbObj>>> map = new HashMap<>();
-        List<Map<String, EmbObj>> lst = new ArrayList<>();
+        Map<String, List<Map<String, EmbObj >>> map = new HashMap<>();
+        List<Map<String, EmbObj >> lst = new ArrayList<>();
         Map<String, EmbObj> map2 = new HashMap<>();
         map2.put("map2-v1", new EmbObj("test1", 741));
         map2.put("map2-v2", new EmbObj("test2", 742));
@@ -319,8 +288,6 @@ public class MapListTest extends MorphiumTestBase {
         map2.put("map3-v4", new EmbObj("test8", 1754));
         lst.add(map2);
         map.put("list2", lst);
-
-
         lst = new ArrayList<>();
         map2 = new HashMap<>();
         map2.put("map1-v1", new EmbObj("test1", 741));
@@ -337,20 +304,19 @@ public class MapListTest extends MorphiumTestBase {
         lst.add(map2);
         map.put("list1", lst);
         o.setMap6a(map);
-
         morphium.store(o);
-
+        Thread.sleep(100);
         CMapListObject ml = morphium.findById(CMapListObject.class, o.getId());
         //Map->List->Map->EmbObj
-        assert (ml.getMap6a().get("list1").get(0).get("map1-v2").getTest().equals("test2"));
+        assert(ml.getMap6a().get("list1").get(0).get("map1-v2").getTest().equals("test2"));
     }
 
     @Test
-    public void complexMapTest() {
+    public void complexMapTest() throws InterruptedException {
         MapListObject o = new MapListObject();
         o.setMapValue(UtilsMap.of("Testvalue", (Object) UtilsMap.of("$lte", "@123")));
         morphium.save(o);
-
+        Thread.sleep(100);
         o = morphium.reread(o);
         assertTrue(o.getMapValue().containsKey("Testvalue"));
     }
@@ -388,80 +354,80 @@ public class MapListTest extends MorphiumTestBase {
 
     @DefaultReadPreference(ReadPreferenceLevel.PRIMARY)
     public static class CMapListObject extends MapListObject {
-        private Map<String, List<EmbObj>> map1;
+        private Map<String, List<EmbObj >> map1;
         private Map<String, EmbObj> map2;
-        private Map<String, List<String>> map3;
-        private Map<String, List<EmbObj>> map4;
+        private Map<String, List<String >> map3;
+        private Map<String, List<EmbObj >> map4;
 
-        private Map<String, Map<String, String>> map5;
-        private Map<String, Map<String, EmbObj>> map5a;
-        private Map<String, List<Map<String, EmbObj>>> map6a;
+        private Map<String, Map<String, String >> map5;
+        private Map<String, Map<String, EmbObj >> map5a;
+        private Map<String, List<Map<String, EmbObj >>> map6a;
 
-        private List<Map<String, String>> map7;
-        private List<List<Map<String, String>>> map7a;
+        private List<Map<String, String >> map7;
+        private List<List<Map<String, String >>> map7a;
 
-        public List<List<Map<String, String>>> getMap7a() {
+        public List<List<Map<String, String >>> getMap7a() {
             return map7a;
         }
 
-        public void setMap7a(List<List<Map<String, String>>> map7a) {
+        public void setMap7a(List<List<Map<String, String >>> map7a) {
             this.map7a = map7a;
         }
 
-        public Map<String, List<Map<String, EmbObj>>> getMap6a() {
+        public Map<String, List<Map<String, EmbObj >>> getMap6a() {
             return map6a;
         }
 
-        public void setMap6a(Map<String, List<Map<String, EmbObj>>> map6a) {
+        public void setMap6a(Map<String, List<Map<String, EmbObj >>> map6a) {
             this.map6a = map6a;
         }
 
-        public Map<String, Map<String, EmbObj>> getMap5a() {
+        public Map<String, Map<String, EmbObj >> getMap5a() {
             return map5a;
         }
 
-        public void setMap5a(Map<String, Map<String, EmbObj>> map5a) {
+        public void setMap5a(Map<String, Map<String, EmbObj >> map5a) {
             this.map5a = map5a;
         }
 
-        public List<Map<String, String>> getMap7() {
+        public List<Map<String, String >> getMap7() {
             return map7;
         }
 
-        public void setMap7(List<Map<String, String>> map7) {
+        public void setMap7(List<Map<String, String >> map7) {
             this.map7 = map7;
         }
 
 
-        public Map<String, Map<String, String>> getMap5() {
+        public Map<String, Map<String, String >> getMap5() {
             return map5;
         }
 
-        public void setMap5(Map<String, Map<String, String>> map5) {
+        public void setMap5(Map<String, Map<String, String >> map5) {
             this.map5 = map5;
         }
 
-        public Map<String, List<EmbObj>> getMap4() {
+        public Map<String, List<EmbObj >> getMap4() {
             return map4;
         }
 
-        public void setMap4(Map<String, List<EmbObj>> map4) {
+        public void setMap4(Map<String, List<EmbObj >> map4) {
             this.map4 = map4;
         }
 
-        public Map<String, List<String>> getMap3() {
+        public Map<String, List<String >> getMap3() {
             return map3;
         }
 
-        public void setMap3(Map<String, List<String>> map3) {
+        public void setMap3(Map<String, List<String >> map3) {
             this.map3 = map3;
         }
 
-        public Map<String, List<EmbObj>> getMap1() {
+        public Map<String, List<EmbObj >> getMap1() {
             return map1;
         }
 
-        public void setMap1(Map<String, List<EmbObj>> map1) {
+        public void setMap1(Map<String, List<EmbObj >> map1) {
             this.map1 = map1;
         }
 
