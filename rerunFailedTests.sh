3cd70c1ad1d3b7cf5a7d46c16ca9be0ed43f5c08
Stephan Bösebeck <sb@caluga.de>
2025-09-14 13:31:09 +0200

    fixing rerrun

--- rerunFailedTests.sh
+++ rerunFailedTests.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
+# Forwarding options to runtests.sh
+includeTags=""
+excludeTags=""
+driver=""
+uri=""
+useExternal=0
+verbose=0
+
+pattern=""
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+    --tags)
+      shift; includeTags="$1"; shift ;;
+    --exclude-tags)
+      shift; excludeTags="$1"; shift ;;
+    --driver)
+      shift; driver="$1"; shift ;;
+    --uri)
+      shift; uri="$1"; shift ;;
+    --external)
+      useExternal=1; shift ;;
+    --verbose)
+      verbose=1; shift ;;
+    --help|-h)
+      echo "Usage: $0 [--tags LIST] [--exclude-tags LIST] [--driver NAME] [--uri URI] [--external] [--verbose] [PATTERN]"
+      exit 0 ;;
+    *)
+      # treat as grep pattern for class names
+      pattern="$1"; shift ;;
+  esac
+done
+
 failed=$(./getStats.sh --noreason --nosum)
 
-if [ ! -z "$1" ]; then
-  failed=$(./getStats.sh --noreason --nosum | grep "$1")
-else
-  failed=$(./getStats.sh --noreason --nosum)
+if [ -n "$pattern" ]; then
+  failed=$(echo "$failed" | grep "$pattern")
 fi
 RD='\033[0;31m'
 GN='\033[0;32m'
@@ -28,5 +59,12 @@ for f in $failed; do
   fi
   #m=$(echo "$m" | sed -e 's/\\(.*$//' )
   echo -e "${YL}Re-Running$CL tests in $BL$cls$CL Method $GN$m$CL"
-  ./runtests.sh --retry 0 --nodel $cls $m
+  args=(--retry 0 --nodel)
+  [ -n "$includeTags" ] && args+=(--tags "$includeTags")
+  [ -n "$excludeTags" ] && args+=(--exclude-tags "$excludeTags")
+  [ -n "$driver" ] && args+=(--driver "$driver")
+  [ -n "$uri" ] && args+=(--uri "$uri")
+  [ "$useExternal" -eq 1 ] && args+=(--external)
+  [ "$verbose" -eq 1 ] && args+=(--verbose)
+  ./runtests.sh "${args[@]}" $cls $m
 done
