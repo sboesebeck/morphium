4694877a4f8865816d3e6bd0fd50fa982d4d55b5
Stephan Bösebeck <sb@caluga.de>
2025-09-26 14:44:36 +0200

    fixing rerun

--- rerunSingleFailed.sh
+++ rerunSingleFailed.sh
@@ -15,6 +15,9 @@ tst=$(for i in test.log/*.log; do
   for m in $(cat $i | grep "\\[ERROR\\]" | grep -A 10 "\\[ERROR\\] Failures:" | grep "\\[ERROR\\]   " | cut -f1 -d: | cut -c11- | cut -f1 -d"»" | cut -f1 -d"("); do
     echo "$pkg.$m"
   done
+  for m in $(cat $i | grep "\\[ERROR\\]" | grep -A 10 "\\[ERROR\\] Errors:" | grep "\\[ERROR\\]   " | cut -f1 -d: | cut -c11- | cut -f1 -d"»" | cut -f1 -d"("); do
+    echo "$pkg.$m"
+  done
 done | fzf)
 #tst=$(cat test.log/*.log | grep "\\[ERROR\\]" | grep -A 10 "\\[ERROR\\] Failures:" | grep "\\[ERROR\\]   " | cut -f1 -d: | cut -c11- | sort | fzf)
 echo "Chose $tst"
