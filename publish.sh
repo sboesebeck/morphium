89b43133aec0f25b348df9b5b7bdcff4af74bede
Stephan Bösebeck <sb@caluga.de>
2025-10-16 09:04:50 +0200

    auto deploy

--- publish.sh
+++ publish.sh
@@ -5,15 +5,15 @@
 branch=$(git symbolic-ref --short HEAD)
 
 if [ $branch == "master" ]; then
-    echo "Do not release from master branch"
-    exit 1
+  echo "Do not release from master branch"
+  exit 1
 fi
 
 if [ $branch != "develop" ]; then
-    echo "you should only run publish from develop branch? Continue? <enter> | CTRL-C"
-    read
+  echo "you should only run publish from develop branch? Continue? <enter> | CTRL-C"
+  read
 fi
-mvn clean compile > /dev/null || exit 1
+mvn clean compile >/dev/null || exit 1
 
 mvn clean release:clean release:prepare || exit 1
 version=$(grep "project.rel.de.caluga\\\\\\:morphium" release.properties | cut -f2 -d=)
@@ -27,11 +27,11 @@ git checkout master
 git merge $tag
 git push
 
+./publish_to_central.sh
+
 #./create_bundle >> test.log/release.log
 git checkout develop
 git push
 #. ./slackurl.inc
 #curl -X POST -H 'Content-type: application/json' --data "{'text':'Deployed current $version to sonatype oss and maven central'}" $SLACKURL
 # mvn nexus-staging:release -Ddescription="Latest release" -DstagingRepositoryId="sonatype-nexus-staging" ||
-
-
