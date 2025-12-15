9251348ece601704ffcfaf1f9434321f023f48ab
Stephan Bösebeck <sb@caluga.de>
2025-12-15 17:16:11 +0100

    minor tweaks

--- showlog.sh
+++ showlog.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 if [ "q$1" = "q--failed" ]; then
   f=$(./runtests.sh --stats --noreason --nosum | grep -v "Calculating" | fzf)
-  bat test.log/$f.log
+  bat $(find test.log -name $f.log)
 elif [ "q$1" = "q--tail" ]; then
 
   tail -n 100 -f $(FZF_DEFAULT_COMMAND="fd --hidden --exclude .git --no-ignore-vcs . test.log/" fzf)
