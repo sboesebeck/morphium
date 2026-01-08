10d4fd21f8bacad82e19f12f0b8728aca4a5e7c9
Stephan Bösebeck <sb@caluga.de>
2026-01-08 23:09:37 +0100

    timout

--- src/test/java/de/caluga/test/mongo/suite/inmem/ChangeStreamInMemTest.java
+++ src/test/java/de/caluga/test/mongo/suite/inmem/ChangeStreamInMemTest.java
@@ -52,18 +52,18 @@ public class ChangeStreamInMemTest extends MorphiumInMemTestBase {
             log.info("===========");
             return run[0];
         });
-        TestUtils.waitForConditionToBecomeTrue(2000, "Change stream listener not ready",
-            () -> true); // Give listener time to start
+        // Give change stream listener time to start - under parallel load this can take longer
+        Thread.sleep(2000);
         morphium.store(new UncachedObject("test", 123));
         ComplexObject o = new ComplexObject();
         o.setEinText("Text");
         morphium.store(o);
         log.info("waiting for some time!");
-        TestUtils.waitForConditionToBecomeTrue(5000, "Expected 2 change events but got " + count,
+        TestUtils.waitForConditionToBecomeTrue(15000, "Expected 2 change events but got " + count,
             () -> count == 2);
         run[0] = false;
         morphium.set(morphium.createQueryFor(UncachedObject.class).f("counter").eq(123), "counter", 7777);
-        TestUtils.waitForConditionToBecomeTrue(3000, "Expected 3 change events but got " + count,
+        TestUtils.waitForConditionToBecomeTrue(10000, "Expected 3 change events but got " + count,
             () -> count == 3); //the listener needs to be called to return false ;-)
         morphium.store(new UncachedObject("test", 123)); //to have the monitor stop
         assert(3 == count) : "Count wrong " + count + "!=3";
