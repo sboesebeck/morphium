9bac33491a7e851c3e3ef3792b8d106469db40c1
Stephan Bösebeck <sb@caluga.de>
2026-01-15 11:42:36 +0100

    test helper scripts update

--- scripts/viewTestlogs.sh
+++ scripts/viewTestlogs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 files=""
-for j in $(for i in test.log/slot_*/slot.log test.log/*.log; do
+for j in $(for i in test.log/slot_*/slot.log test.log/slot_*/*.log test.log/*.log; do
   n=${i%%/slot.log}
   n=${n##*_}
-  f=$(tail -n1 $i | grep -v "completed" | cut -f 2 -d: | cut -f3 -d' ')
+  if echo "$i" | grep "slot.*/slot.log" >/dev/null; then
+    continue
+  fi
+  f=$(tail -n1 $i | grep -v "completed" | grep -v "Help 1" | cut -f 2 -d: | cut -f3 -d' ')
   if [ -z "$f" ]; then
     continue
   fi
