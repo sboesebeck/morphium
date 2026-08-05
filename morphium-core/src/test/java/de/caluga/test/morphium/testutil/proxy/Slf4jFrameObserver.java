08303e34cf4986477fd97fa6b3b8cc8116acde94
Stephan Bösebeck <sb@caluga.de>
2026-08-05 16:29:53 +0200

    test(failover): hello/isMaster address rewriter + example frame observer

---
+++ morphium-core/src/test/java/de/caluga/test/morphium/testutil/proxy/Slf4jFrameObserver.java
@@ -0,0 +1,16 @@
+package de.caluga.test.morphium.testutil.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Trivial working example of a {@link FrameObserver} - the "logging is provided for" seam
+ * the design spec describes. Not used by the failover test itself (which relies only on the
+ * escape-guard assertion, not a wire log), but proves the hook is usable by a real consumer. */
+public class Slf4jFrameObserver implements FrameObserver {
+    private static final Logger log = LoggerFactory.getLogger(Slf4jFrameObserver.class);
+
+    @Override
+    public void onFrame(Direction dir, de.caluga.morphium.driver.wireprotocol.WireProtocolMessage msg, ConnectionCtx ctx) {
+        log.debug("{} frame on {}: {}", dir, ctx, msg);
+    }
+}
